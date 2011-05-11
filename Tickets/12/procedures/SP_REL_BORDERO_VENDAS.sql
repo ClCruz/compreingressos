ac312f1290e7d1edac80d8a044b346fb62a16237
Gabriel Monteiro <gabriel.monteiro@intuiti.com.br>
2011-05-11 18:14:56 +0000

    #12 adição da notação de borderôs para a versão resumida
    git-svn-id: https://portal.cc.com.br:8084/svn/projetos@394 236fd033-a1ae-4692-b96d-e3c3cfb81559

--- Tickets/12/procedures/SP_REL_BORDERO_VENDAS.sql
+++ Tickets/12/procedures/SP_REL_BORDERO_VENDAS.sql
@@ -2516,3 +2516,79 @@ ELSE
 		0 AS Total'
 --print @query
 exec (@query)
+
+
+
+
+GO
+/****** Object:  StoredProcedure [dbo].[SP_REL_BORDERO_VENDAS];14    Script Date: 04/26/2011 09:19:19 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/* EQUIVALENTE AO ;10 (SOMENTE PARA OBTER O NÚMERO DOS BORDERÔS) */
+ALTER    procedure [dbo].[SP_REL_BORDERO_VENDAS];14
+	@Login			varchar(10),
+	@CodPeca		int		= null,
+	@CodSala		varchar(5)	= null,
+	@DataIni		varchar(10)	= null,
+	@DataFim		varchar(10)	= null,
+	@HorSessao		varchar(10)	= null,
+	@NomBase		varchar(30)	= null
+
+ AS
+
+declare @query varchar(8000)
+declare @hora varchar(1000)
+declare @horaAux2 varchar(1000)
+
+if @HorSessao = '' or @HorSessao = 'null' or @HorSessao is null or @HorSessao = '--'
+begin
+	set @hora = ''
+	set @horaAux2 = ''
+end
+else
+begin
+	set @hora = 'and	(tbAp.HorSessao = ''' + convert(varchar(5),@HorSessao) + ''' or ''' + convert(varchar(5),@HorSessao) + ''' is null)'
+	set @horaAux2 = 'and	(tbAp.HorSessao = @horaAux or @horaAux is null)'
+end
+set @query = 
+
+'
+declare @horaAux varchar(10)
+declare @hora varchar(10)
+
+declare @DtIAux Datetime
+declare @DtI varchar(10)
+
+declare @DtFAux Datetime
+declare @DtF varchar(10)
+
+
+set @hora = ''' + @HorSessao + '''
+set @horaAux = case when @hora <> ''null'' then @hora else null end
+
+set @DtI = ''' + @DataIni + '''
+set @DtIAux = case when @DtI <> ''null'' then @DtI else null end
+
+set @DtF = ''' + @DataFim + '''
+set @DtFAux = case when @DtF <> ''null'' then @DtF else null end
+
+set nocount on;
+
+select
+		distinct tbAp.NumBordero
+	from ' + @NomBase + '..tabApresentacao tbAp (nolock)
+
+	inner join ' + @NomBase + '..tabPeca tbPc (nolock)
+		on	tbPc.CodPeca = tbAp.CodPeca
+
+	where		(tbAp.CodPeca = ' + convert(varchar(10),@CodPeca) + ' or ' + convert(varchar(10),@CodPeca) + ' is null)
+		and	(convert(varchar(10), tbAp.DatApresentacao, 112) between  ''' + @DataIni + ''' and ''' + @DataFim + ''' or ''' + @DataIni + ''' is null)
+		' + @hora + '
+		and 	(tbAp.DatApresentacao >= @DtIAux or @DtIAux is null)
+		and 	(tbAp.DatApresentacao <= @DtFAux or @DtFAux is null)
+		' + @horaAux2 + '
+order by NumBordero'
+--print (@query)
+exec (@query)

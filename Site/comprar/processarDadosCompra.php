2d772077f11d3058556223faa6747673dc47bd42
Gabriel Monteiro <gabriel.monteiro@intuiti.com.br>
2011-08-05 14:08:45 +0000

    #96 isolado o parâmetro de teste do IPAGARE
    git-svn-id: https://portal.cc.com.br:8084/svn/projetos@706 236fd033-a1ae-4692-b96d-e3c3cfb81559

--- Site/comprar/processarDadosCompra.php
+++ Site/comprar/processarDadosCompra.php
@@ -69,7 +69,9 @@ $parametros['valor_total'] = 0; //soma total após listagem dos itens
 $parametros['chave'] = '';//chave após o total
 
 //Parâmetros opcionais
-$parametros['teste'] = $is_teste; //Indica que as transações deste pedido serão de teste. Atenção: não enviar este parâmetro se o pedido for de produção.
+if ($is_teste == '1') {
+	$parametros['teste'] = $is_teste; //Indica que as transações deste pedido serão de teste. Atenção: não enviar este parâmetro se o pedido for de produção.
+}
 $parametros['codigo_pedido'] = '';//Código ou chave única do pedido no Site - após a chave
 $parametros['idioma'] = 'pt'; //Utilize 'pt' para português, 'es' para espanhol e 'en' para inglês. Caso não seja informado, português será o idioma padrão.
 

ae06feb1294c3dbff7a59a4639da0ac67a287d66
Jefferson Ferreira <jefferson.ferreira@intuiti.com.br>
2011-08-05 14:39:40 +0000

    git-svn-id: https://portal.cc.com.br:8084/svn/projetos@709 236fd033-a1ae-4692-b96d-e3c3cfb81559

---
+++ Tickets/96-Em produção em 05_08_2011/settings/settings.php
@@ -0,0 +1,18 @@
+<?php
+$nomeSite = 'COMPREINGRESSOS.COM';
+$title = $nomeSite;// . ' - Painel Administrativo';
+
+$cookieExpireTime = time() + 60 * 20; //20min
+
+$compraExpireTime = 15;//minutos
+
+$maxIngressos = 10;//maximo por compra
+
+$uploadPath = '../images/uploads/';
+
+$is_teste = '0';
+
+if (isset($_REQUEST['var'])) {
+	echo $$_REQUEST['var'];
+}
+?>

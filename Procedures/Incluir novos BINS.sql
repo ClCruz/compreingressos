227a93573f39c8ad388d74ffc42eaec9de507ece
Jefferson Ferreira <jefferson.ferreira@intuiti.com.br>
2012-10-25 19:45:26 +0000

    Incluido a tabela temp com a carga do excel enviado pela compreingressos
    git-svn-id: https://portal.cc.com.br:8084/svn/projetos@1219 236fd033-a1ae-4692-b96d-e3c3cfb81559

--- Procedures/Incluir novos BINS.sql
+++ Procedures/Incluir novos BINS.sql
@@ -5,11 +5,24 @@ select * from mw_patrocinador
 
 --2o. verificar se a bandeira do cartão já está cadastrada
 select * from mw_bandeira_cartao 
-
+insert into mw_bandeira_cartao values ('N/I')
 --3o. incluir o novo registro de acordo com a relação recebida da Compreingressos
+
 --exemplo: insert into mw_cartao_patrocinado 
 --        (id_patrocinador, ds_cartao_patrocinado, cd_bin, id_bandeira_cartao)
 --         values (1,'TIM ITAUCARD GOLD MASTERCARD','526892',2)
+select * from mw_cartao_patrocinado
+
+commit
+insert into mw_cartao_patrocinado
+select 1, left([Descrição do Produto],50), [BIN Itaú], b.id_bandeira_cartao
+from temp t
+inner join
+mw_bandeira_cartao b
+on upper(b.ds_bandeira_cartao) = upper(ltrim(t.[bandeira]))
+where not exists (select 1 from mw_cartao_patrocinado c
+					where [BIN Itaú] =cd_bin)
+					and [BIN Itaú] is not null
 
 --4o. incluir o novo registro na mw_evento_patrocinado, relacionando o mw_evento e a mw_cartao_patrocinado
 --Exemplo:insert into mw_evento_patrocinado 

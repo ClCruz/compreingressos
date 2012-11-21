69de4e7b98b8b77d55daf7434a59429c8378c119
Gabriel Monteiro <gabriel.monteiro@intuiti.com.br>
2012-11-21 16:41:42 +0000

    #202
    git-svn-id: https://portal.cc.com.br:8084/svn/projetos@1226 236fd033-a1ae-4692-b96d-e3c3cfb81559

---
+++ Tickets/202/settings/settings.php
@@ -0,0 +1,25 @@
+<?php
+$nomeSite = 'COMPREINGRESSOS.COM';
+$title = $nomeSite;// . ' - Painel Administrativo';
+
+$cookieExpireTime = time() + 60 * 20; //20min
+
+$compraExpireTime = 15;//minutos
+
+$maxIngressos = 10;//maximo por compra
+
+$uploadPath = '../images/uploads/';
+
+$is_teste = '0';
+
+$isContagemAcessos = true;
+
+$recaptcha = array(
+	'private_key' => '6LeWX9kSAAAAAMMPzga35flM8MjChV29_VSxYtlC',
+	'public_key' => '6LeWX9kSAAAAACmAZZ4Gv368Vk7ARc7xLV4mNuW7'
+);
+
+if (isset($_REQUEST['var'])) {
+	echo $$_REQUEST['var'];
+}
+?>

618bdc4cd56d4d28c4b8d6068a898620289bbea6
Gabriel Monteiro <gabriel.monteiro@intuiti.com.br>
2012-12-18 17:04:29 +0000

    #213
    git-svn-id: https://portal.cc.com.br:8084/svn/projetos@1294 236fd033-a1ae-4692-b96d-e3c3cfb81559

--- Site/comprar/antiFraude.php
+++ Site/comprar/antiFraude.php
@@ -1,16 +1,23 @@
 <?php
-$cartao = $_POST['numCartao'][0].$_POST['numCartao'][1].$_POST['numCartao'][2].$_POST['numCartao'][3];
+//se for operador ignorar as regras anti-fraude
+if (!isset($_SESSION['operador'])) {
 
-// Ticket #213 (https://portal.cc.com.br:8084/projetos/ticket/213)
-$query = "SELECT TOP 1 ID_PEDIDO_VENDA FROM MW_PEDIDO_VENDA
-			WHERE IN_SITUACAO = 'F'
-			AND ID_USUARIO_CALLCENTER IS NULL
-			AND DATEADD(HOUR, 1, DT_PEDIDO_VENDA) > GETDATE()
-			AND (ID_CLIENTE = ? AND CD_BIN_CARTAO NOT LIKE ? + '??????' + ? OR ID_CLIENTE <> ? AND ID_IP = ?)";
-$params = array($_SESSION['user'], substr($cartao, 0, 6), substr($cartao, -4), $_SESSION['user'], $_SERVER["REMOTE_ADDR"]);
-$rows = numRows($mainConnection, $query, $params);
+	$cartao = $_POST['numCartao'][0].$_POST['numCartao'][1].$_POST['numCartao'][2].$_POST['numCartao'][3];
 
-if ($rows > 0) {
-	header("Location: pagamento_cancelado.php?erro=539");
-	die();
+	// Ticket #213 (https://portal.cc.com.br:8084/projetos/ticket/213)
+	$query = "SELECT TOP 1 ID_PEDIDO_VENDA FROM MW_PEDIDO_VENDA
+				WHERE IN_SITUACAO = 'F'
+				AND ID_USUARIO_CALLCENTER IS NULL
+				AND DATEADD(HOUR, 1, DT_PEDIDO_VENDA) > GETDATE()
+				AND (ID_CLIENTE = ? AND (CD_BIN_CARTAO <> ? AND CD_BIN_CARTAO <> ?) OR ID_CLIENTE <> ? AND ID_IP = ?)";
+	$params = array($_SESSION['user'], substr($cartao, 0, 6) . '******' . substr($cartao, -4), $cartao, $_SESSION['user'], $_SERVER["REMOTE_ADDR"]);
+	$rows = numRows($mainConnection, $query, $params);
+
+	//print_r(array('<pre>', $query, $params, '</pre>'));die();
+
+	if ($rows > 0) {
+		header("Location: pagamento_cancelado.php?erro=539");
+		die();
+	}
+
 }

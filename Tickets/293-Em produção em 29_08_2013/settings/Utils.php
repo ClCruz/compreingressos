7b6500c535d3608d88815663dd446bdea5d725ad
Jefferson Ferreira <jefferson.ferreira@intuiti.com.br>
2013-08-29 12:52:15 +0000

    git-svn-id: https://portal.cc.com.br:8084/svn/projetos@1722 236fd033-a1ae-4692-b96d-e3c3cfb81559

---
+++ Tickets/293-Em produção em 29_08_2013/settings/Utils.php
@@ -0,0 +1,175 @@
+<?php
+
+/**
+ * Conjunto de funções de uso geral.
+ * @author Edicarlos Barbosa <edicarlosbarbosa@gmail.com>
+ * @since 23-08-2011 14:00
+ * @version 1.0.0
+ * @license GNU GENERAL PUBLIC LICENSE
+ */
+function tratarData($data) {
+  if ($data != "")
+    $data = explode("/", $data);
+  else
+    $data = explode(date('d/m/Y'));
+  return $data[2] . $data[1] . $data[0];
+}
+
+function formatarValor($value) {
+  return (is_null($value)) ? '' : $value;
+}
+
+function formatarValorNumerico($value) {
+  return (is_null($value)) ? '' : number_format($value, 2, ',', '.');
+}
+
+function gerarNotacaoIntervalo($arrayNums) {
+  $str = '';
+  $lastNum = '';
+
+  foreach ($arrayNums as $i) {
+    if ($lastNum === '') {
+      $str .= $i;
+    } else {
+      if ($i === $lastNum + 1 && substr($str, -1) !== '-') {
+        $str .= '-';
+      } else if ($i !== $lastNum + 1 && substr($str, -1) === '-') {
+        $str .= $lastNum . ', ' . $i;
+      } else if ($i !== $lastNum + 1) {
+        $str .= ', ' . $i;
+      }
+    }
+    $lastNum = $i;
+  }
+  if (substr($str, -1) === '-')
+    $str .= $lastNum;
+
+  return $str;
+}
+
+function retornaData($Data) {
+  if (!checkdate($Data))
+    return "";
+  else {
+    $dia = $Data;
+    $mes = $Data;
+    $ano = $Data;
+    return $ano . $mes . $dia;
+  }
+}
+
+function textToDate($date) {
+  $dia = substr($date, 6, 2);
+  $mes = substr($date, 4, 2);
+  $ano = substr($date, 0, 4);
+  return $dia . "/" . $mes . "/" . $ano;
+}
+
+function DiaSemana($Data) {
+  switch ($Data) {
+    case 1:
+      $DiaSemana = "SEGUNDA-FEIRA";
+      break;
+    case 2:
+      $DiaSemana = "TERÇA-FEIRA";
+      break;
+    case 3:
+      $DiaSemana = "QUARTA-FEIRA";
+      break;
+    case 4:
+      $DiaSemana = "QUINTA-FEIRA";
+      break;
+    case 5:
+      $DiaSemana = "SEXTA-FEIRA";
+      break;
+    case 6:
+      $DiaSemana = "SÁBADO";
+      break;
+    case 7:
+      $DiaSemana = "DOMINGO";
+      break;
+  }
+  return $DiaSemana;
+}
+
+function formatarConteudoVazio($valor) {
+  return empty($valor) ? '-' : $valor;
+}
+
+function search_value_presentation($apresentacoes, $date, $canal) {
+  $resultado = array(0, 0);
+  foreach($apresentacoes as $key => $apresentacao){
+    $dateDb = $apresentacao->data . $apresentacao->hora;
+    if ((strcmp($dateDb, $date) == 0) && (strcmp($apresentacao->canal, $canal) == 0)) {
+      $resultado[0] = $apresentacao->qtde;
+      $resultado[1] = $apresentacao->valor;
+    }
+  }
+  return $resultado;
+}
+
+/**
+ * Verifica se o valor é diferente de vazio.
+ * @param String $value
+ * @return String
+ */
+function chk_value($value) {
+  if ((isset($value)) && (!empty($value))) {
+    return $value;
+  } else {
+    return "";
+  }
+}
+
+/**
+ * Verifica se o valor é diferente de vazio. <br/>
+ * Porém retorna uma string "null" caso seja.
+ * @param String $value
+ * @return String
+ */
+function chk_null($value) {
+  if ((isset($value)) && (!empty($value))) {
+    return $value;
+  } else {
+    return "null";
+  }
+}
+
+function arrayCopy(array $array) {
+  $result = array();
+  foreach ($array as $key => $val) {
+    if (is_array($val)) {
+      $result[$key] = arrayCopy($val);
+    } elseif (is_object($val)) {
+      $result[$key] = clone $val;
+    } else {
+      $result[$key] = $val;
+    }
+  }
+  return $result;
+}
+
+/**
+ * Altera a formatação da data para o padrão "mm/dd/yyyy".
+ * @param String $date
+ * @return String
+ */
+function getDateF($date){
+  if ($date != ""){
+    $data = explode("/", $date);
+  }else{
+    $data = explode("/", date('y/m/d'));
+  }
+  $retorno = $data[1] ."/". $data[2] ."/". $data[0];
+  return $retorno;
+}
+
+/**
+ * Remove os caracteres "." e "-" do valor passado por parametro.<br/> 
+ * @param String $doc
+ * @return String
+ */
+function cleanDocuments($doc){
+  return str_replace(array(".","-"), "", $doc);
+}
+?>

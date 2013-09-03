21a2bce8b8a36709b4044c8f728b8c18051e6cab
Edicarlos Barbosa <edicarlos.barbosa@intuiti.com.br>
2013-09-03 17:00:27 +0000

    #319 Ticket Alterado a trigger tr_atualiza_apresentacoes.sql para incluir os campos StaAtivoWeb e StaAtivoBilheteria na lógica de atualização da mw_apresentacao.
    git-svn-id: https://portal.cc.com.br:8084/svn/projetos@1734 236fd033-a1ae-4692-b96d-e3c3cfb81559

--- Triggers/tr_atualiza_apresentacoes.sql
+++ Triggers/tr_atualiza_apresentacoes.sql
@@ -3,139 +3,202 @@
 -- Create date: 08/10/10
 -- Description:	Atualiza as apresentacoes no Middleway
 -- =============================================
-alter TRIGGER dbo.tr_atualiza_apresentacoes 
-   ON  TABAPRESENTACAO
-   AFTER INSERT,DELETE,UPDATE
-AS 
+ALTER TRIGGER dbo.tr_atualiza_apresentacoes ON TABAPRESENTACAO
+AFTER INSERT
+	,DELETE
+	,UPDATE
+AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
+	DECLARE @codpeca SMALLINT
+		,@codapresentacao INT
+		,@datapresentacao SMALLDATETIME
+		,@horsessao CHAR(5)
+		,@nomsala VARCHAR(30)
+		,@id_base INT
+		,@id_evento INT
+		,@in_ativo_web BIT
+		,@in_ativo_bilheteria BIT
 
-	declare @codpeca smallint,
-			@codapresentacao int,
-			@datapresentacao smalldatetime,
-			@horsessao char(5),
-			@nomsala varchar(30),
-			@id_base int,
-			@id_evento int
+	SELECT @id_base = id_base
+	FROM ci_middleway..mw_base
+	WHERE ds_nome_base_sql = DB_NAME()
 
-	select @id_base = id_base from ci_middleway..mw_base where ds_nome_base_sql = DB_NAME()
+	IF @id_base IS NOT NULL
+	BEGIN
+		IF EXISTS (
+				SELECT 1
+				FROM inserted
+				)
+		BEGIN
+			DECLARE C1 CURSOR
+			FOR
+			SELECT a.CodPeca
+				,a.CodApresentacao
+				,a.DatApresentacao
+				,CONVERT(CHAR(5), REPLACE(a.HorSessao, ':', 'h'))
+				,s.nomsala
+				,CASE (a.StaAtivoBilheteria)
+					WHEN 'S'
+						THEN 1
+					ELSE 0
+					END AS StaAtivoBilheteria
+				,CASE (a.StaAtivoWeb)
+					WHEN 'S'
+						THEN 1
+					ELSE 0
+					END AS StaAtivoWeb
+			FROM inserted A
+			INNER JOIN tabSala S ON s.codsala = a.codsala
 
-	if @id_base is not null
-		begin	
-			if exists (Select 1 from inserted)
-				begin
-					DECLARE C1 CURSOR FOR 
-					SELECT  a.CodPeca
-					,		a.CodApresentacao
-					,		a.DatApresentacao
-					,		CONVERT(char(5), REPLACE(a.HorSessao, ':', 'h')) 
-					,		s.nomsala 
-					FROM inserted A inner join tabSala S on s.codsala = a.codsala
+			OPEN C1
 
-					OPEN C1
+			FETCH NEXT
+			FROM C1
+			INTO @codpeca
+				,@codapresentacao
+				,@datapresentacao
+				,@horsessao
+				,@nomsala
+				,@in_ativo_bilheteria
+				,@in_ativo_web
 
-					FETCH NEXT FROM C1 INTO @codpeca
-									,		@codapresentacao
-									,		@datapresentacao
-									,		@horsessao
-									,		@nomsala
-					WHILE @@FETCH_STATUS = 0
-					BEGIN
+			WHILE @@FETCH_STATUS = 0
+			BEGIN
+				SELECT @id_evento = id_evento
+				FROM ci_middleway..mw_evento
+				WHERE id_base = @id_base
+					AND codpeca = @codpeca
 
-						select @id_evento = id_evento from ci_middleway..mw_evento where id_base = @id_base and codpeca = @codpeca
+				IF EXISTS (
+						SELECT 1
+						FROM ci_middleway..mw_apresentacao
+						WHERE id_evento = @id_evento
+							AND codapresentacao = @codapresentacao
+						)
+				BEGIN
+					UPDATE ci_middleway..mw_apresentacao
+					SET dt_apresentacao = @datapresentacao
+						,hr_apresentacao = @horsessao
+						,ds_piso = @nomsala
+						,in_ativo_bilheteria = @in_ativo_bilheteria
+						,in_ativo = @in_ativo_web
+					WHERE id_evento = @id_evento
+						AND codapresentacao = @codapresentacao
 
-						if exists (Select 1 from ci_middleway..mw_apresentacao 
-									where id_evento = @id_evento
-									  and codapresentacao = @codapresentacao)
-							begin
-								UPDATE ci_middleway..mw_apresentacao
-								SET dt_apresentacao = @datapresentacao
-								,	hr_apresentacao = @horsessao
-								,	ds_piso			= @nomsala
-								WHERE id_evento = @id_evento
-								  and codapresentacao = @codapresentacao
-	
-								update ci_middleway..mw_apresentacao_bilhete
-								set in_ativo = 0
-								from
-								inserted as tabapresentacao
-								inner join 
-								ci_middleway..mw_evento as mw_evento
-								on	mw_evento.id_base = @id_base
-								and mw_evento.codpeca = tabapresentacao.codpeca
-								inner join
-								ci_middleway..mw_apresentacao as mw_apresentacao
-								on	mw_apresentacao.id_evento = mw_evento.id_evento
-								and mw_apresentacao.codapresentacao = tabapresentacao.codapresentacao
-								inner join
-								ci_middleway..mw_apresentacao_bilhete ab
-								on ab.id_apresentacao = mw_apresentacao.id_apresentacao
-	
-							end
-						else
-							begin
-								insert into ci_middleway..mw_apresentacao (dt_apresentacao, codapresentacao, hr_apresentacao, id_evento, ds_piso, in_ativo)
-								values (@datapresentacao, @codapresentacao, @horsessao, @id_evento, @nomsala, 1)
-							end
-
-						insert into ci_middleway..mw_apresentacao_bilhete
-						(id_apresentacao, codtipbilhete, ds_tipo_bilhete, vl_preco_unitario, vl_desconto, vl_liquido_ingresso, in_ativo)
-						SELECT 	
-							mw_apresentacao.id_apresentacao,
-							tabtipbilhete.codtipbilhete, 
-							isnull(tabtipbilhete.ds_nome_site,'Não Informado'), 
-							case when isnull(tabtipbilhete.vl_preco_fixo,0) = 0 then tabApresentacao.ValPeca else tabtipbilhete.vl_preco_fixo	end,
-							convert(numeric(15,2), ((case when isnull(tabtipbilhete.vl_preco_fixo,0) = 0 then tabApresentacao.ValPeca else tabtipbilhete.vl_preco_fixo	end) * tabtipbilhete.perdesconto/100)) as valdesconto, 
-							convert(numeric(15,2), (case when isnull(tabtipbilhete.vl_preco_fixo,0) = 0 then tabApresentacao.ValPeca else tabtipbilhete.vl_preco_fixo	end) - ((case when isnull(tabtipbilhete.vl_preco_fixo,0) = 0 then tabApresentacao.ValPeca else tabtipbilhete.vl_preco_fixo	end) * tabtipbilhete.perdesconto/100)) as valliquido,
-							1
-						FROM 
-							inserted as tabapresentacao 
-							inner join  
-							tabvalbilhete
-							on	tabapresentacao.codpeca = tabvalbilhete.codpeca 
-							and tabapresentacao.datapresentacao between tabvalbilhete.datinidesconto and tabvalbilhete.datfindesconto
-							inner join 
-							tabtipbilhete 
-							on	tabtipbilhete.codtipbilhete = tabvalbilhete.codtipbilhete 
-							and	tabtipbilhete.statipbilhete = 'A'  
-							and tabtipbilhete.in_venda_site = '1' 
-							inner join
-							ci_middleway..mw_evento as mw_evento
-							on	mw_evento.id_base = @id_base
-							and mw_evento.codpeca = tabapresentacao.codpeca
-							inner join
-							ci_middleway..mw_apresentacao as mw_apresentacao
-							on	mw_apresentacao.id_evento = mw_evento.id_evento
-							and mw_apresentacao.codapresentacao = tabapresentacao.codapresentacao
-	
-						FETCH NEXT FROM C1 INTO @codpeca
-										,		@codapresentacao
-										,		@datapresentacao
-										,		@horsessao
-										,		@nomsala
-
-					END
-					CLOSE C1
-					DEALLOCATE C1
-				end
-			else
-				begin
-					UPDATE ci_middleway..mw_apresentacao 
+					UPDATE ci_middleway..mw_apresentacao_bilhete
 					SET in_ativo = 0
-					FROM
-						ci_middleway..mw_evento e 
-						inner join 
-						deleted d 
-						on d.codpeca = e.codpeca
-						inner join 
-						ci_middleway..mw_apresentacao a
-						on	a.id_evento = e.id_evento
-					WHERE id_base = @id_base
-					  and a.codapresentacao = @codapresentacao
-				end
-		end
+					FROM inserted AS tabapresentacao
+					INNER JOIN ci_middleway..mw_evento AS mw_evento ON mw_evento.id_base = @id_base
+						AND mw_evento.codpeca = tabapresentacao.codpeca
+					INNER JOIN ci_middleway..mw_apresentacao AS mw_apresentacao ON mw_apresentacao.id_evento = mw_evento.id_evento
+						AND mw_apresentacao.codapresentacao = tabapresentacao.codapresentacao
+					INNER JOIN ci_middleway..mw_apresentacao_bilhete ab ON ab.id_apresentacao = mw_apresentacao.id_apresentacao
+				END
+				ELSE
+				BEGIN
+					INSERT INTO ci_middleway..mw_apresentacao (
+						dt_apresentacao
+						,codapresentacao
+						,hr_apresentacao
+						,id_evento
+						,ds_piso
+						,in_ativo
+						,in_ativo_bilheteria
+						)
+					VALUES (
+						@datapresentacao
+						,@codapresentacao
+						,@horsessao
+						,@id_evento
+						,@nomsala
+						,1
+						,@in_ativo_bilheteria
+						)
+				END
+
+				INSERT INTO ci_middleway..mw_apresentacao_bilhete (
+					id_apresentacao
+					,codtipbilhete
+					,ds_tipo_bilhete
+					,vl_preco_unitario
+					,vl_desconto
+					,vl_liquido_ingresso
+					,in_ativo
+					)
+				SELECT mw_apresentacao.id_apresentacao
+					,tabtipbilhete.codtipbilhete
+					,isnull(tabtipbilhete.ds_nome_site, 'Não Informado')
+					,CASE 
+						WHEN isnull(tabtipbilhete.vl_preco_fixo, 0) = 0
+							THEN tabApresentacao.ValPeca
+						ELSE tabtipbilhete.vl_preco_fixo
+						END
+					,convert(NUMERIC(15, 2), (
+							(
+								CASE 
+									WHEN isnull(tabtipbilhete.vl_preco_fixo, 0) = 0
+										THEN tabApresentacao.ValPeca
+									ELSE tabtipbilhete.vl_preco_fixo
+									END
+								) * tabtipbilhete.perdesconto / 100
+							)) AS valdesconto
+					,convert(NUMERIC(15, 2), (
+							CASE 
+								WHEN isnull(tabtipbilhete.vl_preco_fixo, 0) = 0
+									THEN tabApresentacao.ValPeca
+								ELSE tabtipbilhete.vl_preco_fixo
+								END
+							) - (
+							(
+								CASE 
+									WHEN isnull(tabtipbilhete.vl_preco_fixo, 0) = 0
+										THEN tabApresentacao.ValPeca
+									ELSE tabtipbilhete.vl_preco_fixo
+									END
+								) * tabtipbilhete.perdesconto / 100
+							)) AS valliquido
+					,1
+				FROM inserted AS tabapresentacao
+				INNER JOIN tabvalbilhete ON tabapresentacao.codpeca = tabvalbilhete.codpeca
+					AND tabapresentacao.datapresentacao BETWEEN tabvalbilhete.datinidesconto
+						AND tabvalbilhete.datfindesconto
+				INNER JOIN tabtipbilhete ON tabtipbilhete.codtipbilhete = tabvalbilhete.codtipbilhete
+					AND tabtipbilhete.statipbilhete = 'A'
+					AND tabtipbilhete.in_venda_site = '1'
+				INNER JOIN ci_middleway..mw_evento AS mw_evento ON mw_evento.id_base = @id_base
+					AND mw_evento.codpeca = tabapresentacao.codpeca
+				INNER JOIN ci_middleway..mw_apresentacao AS mw_apresentacao ON mw_apresentacao.id_evento = mw_evento.id_evento
+					AND mw_apresentacao.codapresentacao = tabapresentacao.codapresentacao
+
+				FETCH NEXT
+				FROM C1
+				INTO @codpeca
+					,@codapresentacao
+					,@datapresentacao
+					,@horsessao
+					,@nomsala
+					,@in_ativo_bilheteria
+					,@in_ativo_web
+			END
+
+			CLOSE C1
+
+			DEALLOCATE C1
+		END
+		ELSE
+		BEGIN
+			UPDATE ci_middleway..mw_apresentacao
+			SET in_ativo = 0
+			FROM ci_middleway..mw_evento e
+			INNER JOIN deleted d ON d.codpeca = e.codpeca
+			INNER JOIN ci_middleway..mw_apresentacao a ON a.id_evento = e.id_evento
+			WHERE id_base = @id_base
+				AND a.codapresentacao = @codapresentacao
+		END
+	END
 END
 GO

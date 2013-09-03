7aee26ccaf5dbcdb7a8522370d8fe1bea94cf7db
Edicarlos Barbosa <edicarlos.barbosa@intuiti.com.br>
2013-09-03 17:03:11 +0000

    #319 Ticket Modificado a tela eventosAtivos.php para atualizar o status da apresentação na Bilheteria (tabApresentacao.StaAtivoWeb) após uma alteração.
    git-svn-id: https://portal.cc.com.br:8084/svn/projetos@1735 236fd033-a1ae-4692-b96d-e3c3cfb81559

--- Site/admin/actions/eventosAtivos.php
+++ Site/admin/actions/eventosAtivos.php
@@ -1,25 +1,38 @@
 <?php
+
 if (acessoPermitido($mainConnection, $_SESSION['admin'], 19, true)) {
+  if ($_GET['action'] == 'update' and isset($_GET['codevento'])) {
+    /* ------------ UPDATE ------------ */
+    $_POST["in_ativo"] = ($_POST["in_ativo"] == 'on') ? 1 : 0;
 
-if ($_GET['action'] == 'update' and isset($_GET['codevento'])) { /*------------ UPDATE ------------*/
-	$_POST["in_ativo"] = ($_POST["in_ativo"] == 'on') ? 1 : 0;
-	
-	$query = "UPDATE MW_APRESENTACAO SET IN_ATIVO = ? WHERE ID_APRESENTACAO = ?";
-	$params = array($_POST['in_ativo'], $_POST['codevento']);
-	
-	if (executeSQL($mainConnection, $query, $params)) {
-		$retorno = 'true?codevento='.$_GET['codevento'];
-	} else {
-		$retorno = sqlErrors();
-	}
-	
-} 
+    $query = "UPDATE MW_APRESENTACAO SET IN_ATIVO = ? WHERE ID_APRESENTACAO = ?";
+    $params = array($_POST['in_ativo'], $_POST['codevento']);
 
-if (is_array($retorno)) {
-	echo $retorno[0]['message'];
-} else {
-	echo $retorno;
-}
+    if (executeSQL($mainConnection, $query, $params)) {
+      $queryApresentacao = "SELECT B.DS_NOME_BASE_SQL, I.CODAPRESENTACAO
+	FROM MW_APRESENTACAO I
+	INNER JOIN MW_EVENTO E ON E.ID_EVENTO = I.ID_EVENTO
+	INNER JOIN MW_BASE B ON B.ID_BASE = E.ID_BASE
+        WHERE I.ID_APRESENTACAO = ?";
+      $apresentacao = executeSQL($mainConnection, $queryApresentacao, array($_POST['codevento']), true);
+      $queryTabApresentacao = "UPDATE " . $apresentacao["DS_NOME_BASE_SQL"] . "..TABAPRESENTACAO
+			SET STAATIVOWEB = CASE " . $_POST["in_ativo"] . " WHEN 1 THEN 'S' ELSE 'N' END
+			WHERE CODAPRESENTACAO = " . $apresentacao["CODAPRESENTACAO"];
+      $result = executeSQL($mainConnection, $queryTabApresentacao, array());
+      if($result == true){
+        $retorno = 'true?codevento=' . $_GET['codevento'];
+      }else{
+        $retorno = "Houve um erro ao replicar a atualização na Bilheteria!";
+      }
+    } else {
+      $retorno = sqlErrors();
+    }
+  }
 
+  if (is_array($retorno)) {
+    echo $retorno[0]['message'];
+  } else {
+    echo $retorno;
+  }
 }
 ?>

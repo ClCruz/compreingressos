7f83ffb88edc984770992a85c409a400a765435a
Gabriel Monteiro <gabriel.monteiro@intuiti.com.br>
2013-11-25 13:44:54 +0000

    #361
    git-svn-id: https://portal.cc.com.br:8084/svn/projetos@1936 236fd033-a1ae-4692-b96d-e3c3cfb81559

--- Site/admin/actions/controleEntrada.php
+++ Site/admin/actions/controleEntrada.php
@@ -132,9 +132,9 @@ if (acessoPermitido($mainConnection, $_SESSION['admin'], 320, true)) {
 										and			  iac.id_usuario = ?
 										and			  iac.CodPeca = tbAp.CodPeca
 		            where               tbPc.CodPeca = ?
-					            /*AND CONVERT(DATETIME, CONVERT(VARCHAR(8), TBAP.DATAPRESENTACAO, 112) + ' ' + TBAP.HORSESSAO)
+					            AND CONVERT(DATETIME, CONVERT(VARCHAR(8), TBAP.DATAPRESENTACAO, 112) + ' ' + TBAP.HORSESSAO)
 									>= CONVERT(DATETIME, CONVERT(VARCHAR(8), DATEADD(DAY, -1, GETDATE()), 112) + ' 22:00')
-					            AND TBAP.DATAPRESENTACAO <= GETDATE()*/
+					            AND TBAP.DATAPRESENTACAO <= GETDATE()
 		            group by tbAp.DatApresentacao
 		            order by tbAp.DatApresentacao";
 		$params = array($_GET['cboTeatro'], $_SESSION['admin'], $_GET['cboPeca']);
@@ -162,8 +162,8 @@ if (acessoPermitido($mainConnection, $_SESSION['admin'], 320, true)) {
 										and			  iac.id_usuario = ?
 										and			  iac.CodPeca = tbAp.CodPeca
 		            where       tbPc.CodPeca = ?
-				            /*AND CONVERT(DATETIME, CONVERT(VARCHAR(8), TBAP.DATAPRESENTACAO, 112) + ' ' + TBAP.HORSESSAO)
-				            >= CONVERT(DATETIME, CONVERT(VARCHAR(8), DATEADD(DAY, -1, GETDATE()), 112) + ' 22:00')*/
+				            AND CONVERT(DATETIME, CONVERT(VARCHAR(8), TBAP.DATAPRESENTACAO, 112) + ' ' + TBAP.HORSESSAO)
+				            >= CONVERT(DATETIME, CONVERT(VARCHAR(8), DATEADD(DAY, -1, GETDATE()), 112) + ' 22:00')
 				            AND TBAP.DATAPRESENTACAO = CONVERT(DATETIME, ?, 112)
 		            group by tbAp.HorSessao
 		            order by tbAp.HorSessao";

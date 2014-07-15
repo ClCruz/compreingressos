d6ef12d6a573100516309935f62a4794a2425396
Edicarlos Barbosa <edicarlos.barbosa@intuiti.com.br>
2014-07-15 18:24:26 +0000

    #421 Modificado processo de compra para implementação da funcionalidade de PDV. Adicionado nova tela de login loginPDV exclusiva para usuários do PDV. Inibido as operações ao Braspag quando for venda PDV.
    git-svn-id: https://portal.cc.com.br:8084/svn/projetos@2093 236fd033-a1ae-4692-b96d-e3c3cfb81559

---
+++ Tickets/421/admin/servicosConveniencia.php
@@ -0,0 +1,313 @@
+<?php
+require_once('../settings/functions.php');
+$mainConnection = mainConnection();
+session_start();
+
+function formatNumber ($number) {
+    return number_format($number, 2, ',', '');
+}
+
+function combo_tipo_valor ($name, $valor) {
+    $tipos = array(
+        'V' => 'R$',
+        'P' => '%'
+    );
+
+    $combo = '<select name="' . $name . '" class="inputStyle" id="' . $name . '"><option value="">Selecione um tipo...</option>';
+    foreach ($tipos as $key => $val) {
+        $combo .= '<option value="' . $key . '"' . (($valor == $key) ? ' selected' : '') . '>' . (($number) ? $key : $val) . '</option>';
+    }
+    $combo .= '</select>';
+
+    return $combo;
+}
+
+if (acessoPermitido($mainConnection, $_SESSION['admin'], 6, true)) {
+
+    $pagina = basename(__FILE__);
+
+    if (isset($_GET['action'])) {
+        require('actions/' . $pagina);
+    } else {
+        $result = executeSQL($mainConnection,
+               'SELECT E.DS_EVENTO,
+                CONVERT(VARCHAR(10),
+                T.DT_INICIO_VIGENCIA, 103) DT_INICIO_VIGENCIA,
+                T.IN_TAXA_POR_PEDIDO,
+                T.IN_TAXA_CONVENIENCIA,
+                T.VL_TAXA_CONVENIENCIA,
+                T.VL_TAXA_PROMOCIONAL,
+                T.VL_TAXA_UM_INGRESSO,
+                CASE
+                    WHEN CONVERT(CHAR(8), T.DT_INICIO_VIGENCIA, 112) >= CONVERT(CHAR(8), GETDATE(), 112)
+                        THEN 1
+                        ELSE 0
+                END EDICAO,
+                T.IN_COBRAR_PDV
+                FROM MW_TAXA_CONVENIENCIA T
+                INNER JOIN MW_EVENTO E ON E.ID_EVENTO = T.ID_EVENTO
+                WHERE E.ID_BASE = ?',
+                array($_GET['teatro']));
+
+        $resultTeatros = executeSQL($mainConnection, 'SELECT ID_BASE, DS_NOME_TEATRO FROM MW_BASE WHERE IN_ATIVO = \'1\'');
+?>
+
+        <script type="text/javascript" src="../javascripts/simpleFunctions.js"></script>
+        <script>
+            $(function() {
+                var pagina = '<?php echo $pagina; ?>'
+
+                $('#app table').delegate('a', 'click', function(event) {
+                    event.preventDefault();
+
+                    var $this = $(this),
+                    href = $this.attr('href'),
+                    id = 'idEvento=' + $.getUrlVar('idEvento', href) + '&data=' + $.getUrlVar('data', href),
+                    tr = $this.closest('tr');
+
+                    if (href.indexOf('?action=add') != -1 || href.indexOf('?action=update') != -1) {
+                        if (!validateFields()) return false;
+
+                        $.ajax({
+                            url: href,
+                            type: 'post',
+                            data: $('#dados').serialize(),
+                            success: function(data) {
+                                if (data.substr(0, 4) == 'true') {
+                                    var id = $.serializeUrlVars(data);
+
+                                    tr.find('td:not(.button):eq(0)').html($('#idEvento option:selected').text());
+                                    tr.find('td:not(.button):eq(1)').html($('#data').val());
+                                    tr.find('td:not(.button):eq(2)').html($('#tipo option:selected').text());
+                                    tr.find('td:not(.button):eq(3)').html($('#valor').val());
+                                    tr.find('td:not(.button):eq(4)').html($('#valor2').val());
+                                    tr.find('td:not(.button):eq(5)').html($('#valor3').val());
+                                    tr.find('td:not(.button):eq(6)').html($('#cobrarPorPedido').is(':checked') ? 'sim' : 'n&atilde;o');
+                                    tr.find('td:not(.button):eq(7)').html($('#cobrarNoPDV').is(':checked') ? 'sim' : 'n&atilde;o');
+
+                                    $this.text('Editar').attr('href', pagina + '?action=edit&' + id);
+                                    tr.find('td.button a:last').attr('href', pagina + '?action=delete&' + id);
+                                    tr.removeAttr('id');
+                                } else {
+                                    $.dialog({text: data});
+                                }
+                            }
+                        });
+                    } else if (href.indexOf('?action=edit') != -1) {
+                        if(!hasNewLine()) return false;
+
+                        var values = new Array();
+
+                        tr.attr('id', 'newLine');
+
+                        $.each(tr.find('td:not(.button)'), function() {
+                            values.push($(this).text());
+                        });
+
+                        tr.find('td:not(.button):eq(0)').html('<?php echo comboEvento('idEvento', $_GET['teatro']); ?>');
+			             $('#idEvento option').filter(function(){return $(this).text() == values[0]}).prop('selected', 'selected');
+                        tr.find('td:not(.button):eq(1)').html('<input name="data" type="text" class="datePicker inputStyle" id="data" maxlength="10" value="' + values[1] + '" readonly>');
+                        tr.find('td:not(.button):eq(2)').html('<?php echo combo_tipo_valor('tipo'); ?>');
+                         $('#tipo option').filter(function(){return $(this).text() == values[2]}).prop('selected', 'selected');
+                        tr.find('td:not(.button):eq(3)').html('<input name="valor" type="text" class="inputStyle" id="valor" maxlength="6" value="' + values[3] + '" >');
+                        tr.find('td:not(.button):eq(4)').html('<input name="valor2" type="text" class="inputStyle" id="valor2" maxlength="6" value="' + values[4] + '" >');
+                        tr.find('td:not(.button):eq(5)').html('<input name="valor3" type="text" class="inputStyle" id="valor3" maxlength="6" value="' + values[5] + '" >');
+                        tr.find('td:not(.button):eq(6)').html('<input name="cobrarPorPedido" type="checkbox" class="inputStyle" id="cobrarPorPedido" ' + (values[6] == 'sim' ? 'checked' : ''  )+ ' />');
+                        tr.find('td:not(.button):eq(7)').html('<input name="cobrarNoPDV" type="checkbox" class="inputStyle" id="cobrarNoPDV" ' + (values[7] == 'sim' ? 'checked' : ''  )+ ' />');
+
+                        $this.text('Salvar').attr('href', pagina + '?action=update&' + id);
+
+                        setDatePickers();
+                    } else if (href == '#delete') {
+                        tr.remove();
+                    } else if (href.indexOf('?action=delete') != -1) {
+                        $.confirmDialog({
+                            text: 'Tem certeza que deseja apagar este registro?',
+                            uiOptions: {
+                                buttons: {
+                                    'Sim': function() {
+                                        $(this).dialog('close');
+                                        $.get(href, function(data) {
+                                            if (data.replace(/^\s*/, "").replace(/\s*$/, "") == 'true') {
+                                                tr.remove();
+                                            } else {
+                                                $.dialog({text: data});
+                                            }
+                                        });
+                                    }
+                                }
+                            }
+                        });
+                    }
+                });
+
+                $('#new').button().click(function(event) {
+                    event.preventDefault();
+
+                    if(!hasNewLine()) return false;
+
+                    var newLine = '<tr id="newLine">' +
+                        '<td>' +
+                        '<?php echo comboEvento('idEvento', $_GET['teatro']); ?>' +
+                        '</td>' +
+                        '<td><input name="data" type="text" class="datePicker inputStyle" id="data" maxlength="10" readonly></td>' +
+                        '<td><?php echo combo_tipo_valor('tipo'); ?></td>' +
+                        '<td><input name="valor" type="text" class="number inputStyle" id="valor" maxlength="6" ></td>' +
+                        '<td><input name="valor2" type="text" class="number inputStyle" id="valor2" maxlength="6" ></td>' +
+                        '<td><input name="valor3" type="text" class="number inputStyle" id="valor3" maxlength="6" ></td>' +
+                        '<td><input name="cobrarPorPedido" type="checkbox" class="inputStyle" id="cobrarPorPedido" /></td>' +
+                        '<td><input name="cobrarNoPDV" type="checkbox" class="inputStyle" id="cobrarNoPDV" /></td>' +
+                        '<td class="button"><a href="' + pagina + '?action=add">Salvar</a></td>' +
+                        '<td class="button"><a href="#delete">Apagar</a></td>' +
+                        '</tr>';
+                    $(newLine).appendTo('#app table tbody');
+                    setDatePickers();
+                    $('.datePicker').datepicker('option', 'minDate', 0);
+                });
+
+                $('#teatro').change(function() {
+                    document.location = '?p=' + pagina.replace('.php', '') + '&teatro=' + $(this).val();
+                });
+
+                $('#enviarvalorPorPedido').button().click(function(){
+                    $.confirmDialog({
+                        text: 'Somente os registros marcados como "Cobrar por Pedido" e que ainda não estão em vigência, ou entrarão em vigência hoje, serão alterados.</br></br>Deseja continuar?',
+                        uiOptions: {
+                            buttons: {
+                                'Sim': function() {
+                                    $.ajax({
+                                        url: pagina + '?action=updateAll',
+                                        type: 'post',
+                                        data: $('#dados').serialize(),
+                                        success: function(data) {
+                                            if (data.substr(0, 4) == 'true') {
+                                                document.location = document.location;
+                                            } else {
+                                                $.dialog({text: data});
+                                            }
+                                        }
+                                    });
+                                }
+                            }
+                        }
+                    });
+                });
+
+                function validateFields() {
+                    var idEvento = $('#idEvento'),
+                    data = $('#data'),
+                    valor = $('#valor'),
+                    valor2 = $('#valor2'),
+                    valor3 = $('#valor3'),
+                    tipo = $('#tipo'),
+                    valido = true;
+                    if (idEvento.val() == '') {
+                        idEvento.parent().addClass('ui-state-error');
+                        valido = false;
+                    } else {
+                        idEvento.parent().removeClass('ui-state-error');
+                    }
+                    if (data.val() == '') {
+                        data.parent().addClass('ui-state-error');
+                        valido = false;
+                    } else {
+                        data.parent().removeClass('ui-state-error');
+                    }
+                    if (tipo.val() == '') {
+                        tipo.parent().addClass('ui-state-error');
+                        valido = false;
+                    } else {
+                        tipo.parent().removeClass('ui-state-error');
+                    }
+                    if (valor.val() <= 0) {
+                        valor.parent().addClass('ui-state-error');
+                        valido = false;
+                    } else {
+                        valor.parent().removeClass('ui-state-error');
+                    }
+                    if (valor2.val() <= 0) {
+                        valor2.parent().addClass('ui-state-error');
+                        valido = false;
+                    } else {
+                        valor2.parent().removeClass('ui-state-error');
+                    }
+                    if (valor3.val() <= 0) {
+                        valor3.parent().addClass('ui-state-error');
+                        valido = false;
+                    } else {
+                        valor3.parent().removeClass('ui-state-error');
+                    }
+
+                    return valido;
+                }
+            });
+        </script>
+        <h2>Valor do Servi&ccedil;o</h2>
+        <form id="dados" name="dados" method="post">
+            <p style="width:200px;"><?php echo comboTeatro('teatro', $_GET['teatro']); ?></p>
+
+            <table class="ui-widget ui-widget-content">
+                <thead>
+
+                    <?php if ($_GET['teatro']) { ?>
+                    <tr class="ui-widget-header ">
+                        <th colspan="3" align="right">Valor por compra:</th>
+                        <th colspan="6">
+                            R$ <input type="text" name="valorPorPedido" value="<?php echo $_GET['valorPorPedido']; ?>" />
+                            <input type="button" id="enviarvalorPorPedido" value="Alterar" />
+                        </th>
+                    </tr>
+                    <?php } ?>
+
+                    <tr class="ui-widget-header ">
+                        <th>Evento</th>
+                        <th>Data de In&iacute;cio de Vig&ecirc;ncia</th>
+                        <th>Tipo</th>
+                        <th>Normal</th>
+                        <th>Promocional</th>
+                        <th>Um Ingresso</th>
+                        <th>Cobrar por Pedido</th>
+                        <th>Cobrar no PDV</th>
+                        <th colspan="2">A&ccedil;&otilde;es</th>
+                    </tr>
+                </thead>
+                <tbody>
+<?php
+        while ($rs = fetchResult($result)) {
+            $idEvento = utf8_encode($rs['DS_EVENTO']);
+            $data = $rs['DT_INICIO_VIGENCIA'];
+            $tipo = $rs['IN_TAXA_CONVENIENCIA'];
+            $valor = $rs['VL_TAXA_CONVENIENCIA'];
+            $valor2 = $rs['VL_TAXA_PROMOCIONAL'];
+            $valor3 = $rs['VL_TAXA_UM_INGRESSO'];
+            $cobrarPorPedido = $rs['IN_TAXA_POR_PEDIDO'];
+            $cobrarNoPDV = $rs['IN_COBRAR_PDV']
+?>
+            <tr>
+                <td><?php echo $idEvento; ?></td>
+                <td><?php echo $data; ?></td>
+                <td><?php echo $tipo == 'V' ? 'R$' : '%' ; ?></td>
+                <td><?php echo formatNumber($valor); ?></td>
+                <td><?php echo formatNumber($valor2); ?></td>
+                <td><?php echo formatNumber($valor3); ?></td>
+                <td><?php echo $cobrarPorPedido == 'S' ? 'sim' : 'n&atilde;o'; ?></td>
+                <td><?php echo $cobrarNoPDV == 'S' ? 'sim' : 'n&atilde;o'; ?></td>
+
+<?php if ($rs['EDICAO']) { ?>
+                    <td class="button"><a href="<?php echo $pagina; ?>?action=edit&idEvento=<?php echo urlencode($idEvento); ?>&data=<?php echo $data; ?>">Editar</a></td>
+                    <td class="button"><a href="<?php echo $pagina; ?>?action=delete&idEvento=<?php echo urlencode($idEvento); ?>&data=<?php echo $data; ?>">Apagar</a></td>
+<?php } else { ?>
+                <td colspan="2">&nbsp;</td>
+                <?php } ?>
+            </tr>
+                <?php
+            }
+                ?>
+        </tbody>
+    </table>
+    <a id="new" href="#new">Novo</a>
+</form>
+<?php
+        }
+    }
+?>
---
+++ Tickets/421/comprar/concretizarCompra.php
@@ -0,0 +1,173 @@
+<?php
+$pedido_id = $parametros['OrderData']['OrderId'];
+$braspag_id = $result->AuthorizeTransactionResult->OrderData->BraspagOrderId;
+$braspag_transaction_id = $result->AuthorizeTransactionResult->PaymentDataCollection->PaymentDataResponse->BraspagTransactionId;
+$transaction_id = $result->AuthorizeTransactionResult->PaymentDataCollection->PaymentDataResponse->AcquirerTransactionId;
+$transaction_auth = $result->AuthorizeTransactionResult->PaymentDataCollection->PaymentDataResponse->AuthorizationCode;
+
+if(isset($_SESSION['usuario_pdv']) and $_SESSION['usuario_pdv'] == 1){
+    $meio_pagamento = $_POST['codCartao'];
+}else{
+    $meio_pagamento = $result->AuthorizeTransactionResult->PaymentDataCollection->PaymentDataResponse->PaymentMethod;
+}
+
+
+$query = 'SELECT ID_MEIO_PAGAMENTO
+			 FROM MW_MEIO_PAGAMENTO MP
+			 WHERE CD_MEIO_PAGAMENTO = ?';
+$params = array($meio_pagamento);
+$id_meio_pagamento = executeSQL($mainConnection, $query, $params, true);
+$id_meio_pagamento = $id_meio_pagamento['ID_MEIO_PAGAMENTO'];
+
+$query = 'SELECT TOP 1
+			 C.ID_CLIENTE,
+			 C.DS_NOME,
+			 C.DS_SOBRENOME,
+			 C.DS_DDD_TELEFONE,
+			 C.DS_TELEFONE,
+			 C.DS_NOME,
+			 C.DS_SOBRENOME,
+			 C.CD_CPF,
+			 C.CD_RG,
+			 PV.CD_BIN_CARTAO,
+			 R.ID_SESSION,
+			 R.CD_BINITAU,
+			 PV.ID_USUARIO_CALLCENTER,
+			 PV.IN_RETIRA_ENTREGA
+			 FROM MW_CLIENTE C
+			 INNER JOIN MW_PEDIDO_VENDA PV ON PV.ID_CLIENTE = C.ID_CLIENTE
+			 INNER JOIN MW_ITEM_PEDIDO_VENDA IPV ON IPV.ID_PEDIDO_VENDA = PV.ID_PEDIDO_VENDA
+			 INNER JOIN MW_RESERVA R ON R.ID_RESERVA = IPV.ID_RESERVA
+			 WHERE PV.ID_PEDIDO_VENDA = ?';
+$params = array($pedido_id);
+$dados = executeSQL($mainConnection, $query, $params, true);
+
+$query = 'SELECT DISTINCT A.CODAPRESENTACAO, B.ID_BASE, B.DS_NOME_BASE_SQL
+			 FROM
+			 MW_BASE B
+			 INNER JOIN MW_EVENTO E ON E.ID_BASE = B.ID_BASE
+			 INNER JOIN MW_APRESENTACAO A ON A.ID_EVENTO = E.ID_EVENTO
+			 INNER JOIN MW_ITEM_PEDIDO_VENDA I ON I.ID_APRESENTACAO = A.ID_APRESENTACAO
+			 INNER JOIN MW_PEDIDO_VENDA P ON P.ID_PEDIDO_VENDA = I.ID_PEDIDO_VENDA
+			 WHERE P.ID_CLIENTE = ? AND P.ID_PEDIDO_VENDA = ? AND P.IN_SITUACAO = \'P\'';
+$params = array($dados['ID_CLIENTE'], $pedido_id);
+$result = executeSQL($mainConnection, $query, $params);
+
+$noErrors = true;
+$retornoProcedure = '';
+
+// Definir se cliente busca ingresso
+if ($dados["ID_USUARIO_CALLCENTER"]) {
+	//receber ingresso
+	if ($dados["IN_RETIRA_ENTREGA"] != 'R')
+		$caixa = 252;
+	//buscar ingresso
+	else
+		$caixa = 254;				
+} else {
+	//receber ingresso
+	if ($dados["IN_RETIRA_ENTREGA"] != 'R')
+		$caixa = 253;
+	//buscar ingresso
+	else
+		$caixa = 255;
+}
+
+$query = 'UPDATE MW_PEDIDO_VENDA SET
+            ID_TRANSACTION_BRASPAG = ?,
+            ID_MEIO_PAGAMENTO = ?
+			WHERE ID_PEDIDO_VENDA = ?
+			AND ID_CLIENTE = ?';
+executeSQL($mainConnection, $query, array($braspag_transaction_id, $id_meio_pagamento, $pedido_id, $dados['ID_CLIENTE']));
+
+//beginTransaction($mainConnection);
+
+while ($rs = fetchResult($result) and $noErrors) {
+	$query = 'EXEC '.strtoupper($rs['DS_NOME_BASE_SQL']).'..SP_VEN_INS001_WEB ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?';
+	$params = array($dados['ID_SESSION'], $rs['ID_BASE'], $meio_pagamento, $rs['CODAPRESENTACAO'],
+						 $dados['DS_DDD_TELEFONE'], $dados['DS_TELEFONE'], ($dados['DS_NOME'].' '.$dados['DS_SOBRENOME']),
+						 $dados['CD_CPF'], $dados['CD_RG'], $pedido_id, $braspag_id,
+						 $transaction_auth, $transaction_id, $dados['CD_BIN_CARTAO'],
+						 $caixa);
+	$retornoProcedure = executeSQL($mainConnection, $query, $params, true);
+	$noErrors = ($retornoProcedure[0] and $noErrors);
+}
+
+$sqlErrors = sqlErrors();
+if ($noErrors and empty($sqlErrors)) {
+	executeSQL($mainConnection, 'DELETE MW_RESERVA WHERE ID_SESSION = ?', array($dados['ID_SESSION']));
+	//commitTransaction($mainConnection);
+
+	$json = json_encode(array('retorno' => '6. OK - Pedido = ' . $pedido_id));
+	include('logiPagareChamada.php');
+	
+	ob_clean();
+
+	$query = "SELECT R.ID_RESERVA, R.ID_APRESENTACAO, R.ID_APRESENTACAO_BILHETE, R.DS_LOCALIZACAO AS DS_CADEIRA,
+					R.DS_SETOR, E.ID_EVENTO, E.DS_EVENTO, ISNULL(LE.DS_LOCAL_EVENTO, B.DS_NOME_TEATRO) DS_NOME_TEATRO,
+					CONVERT(VARCHAR(10), A.DT_APRESENTACAO, 103) DT_APRESENTACAO, A.HR_APRESENTACAO,
+					AB.VL_LIQUIDO_INGRESSO, AB.DS_TIPO_BILHETE, E.ID_BASE, A.CodApresentacao, R.CodVenda
+				FROM MW_ITEM_PEDIDO_VENDA R
+				INNER JOIN MW_APRESENTACAO A ON A.ID_APRESENTACAO = R.ID_APRESENTACAO AND A.IN_ATIVO = '1'
+				INNER JOIN MW_EVENTO E ON E.ID_EVENTO = A.ID_EVENTO AND E.IN_ATIVO = '1'
+				INNER JOIN MW_BASE B ON B.ID_BASE = E.ID_BASE
+				INNER JOIN MW_APRESENTACAO_BILHETE AB ON AB.ID_APRESENTACAO_BILHETE = R.ID_APRESENTACAO_BILHETE AND AB.IN_ATIVO = '1'
+				LEFT JOIN MW_LOCAL_EVENTO LE ON E.ID_LOCAL_EVENTO = LE.ID_LOCAL_EVENTO
+				WHERE R.ID_PEDIDO_VENDA = ?
+				ORDER BY E.DS_EVENTO, R.ID_APRESENTACAO, R.DS_LOCALIZACAO";
+	$params = array($pedido_id);
+	$result = executeSQL($mainConnection, $query, $params);
+
+	$queryServicos = "SELECT TOP 1 TC.IN_TAXA_POR_PEDIDO
+					FROM MW_TAXA_CONVENIENCIA TC
+					INNER JOIN MW_PEDIDO_VENDA PV ON PV.DT_PEDIDO_VENDA >= TC.DT_INICIO_VIGENCIA
+					INNER JOIN MW_ITEM_PEDIDO_VENDA IPV ON IPV.ID_PEDIDO_VENDA = PV.ID_PEDIDO_VENDA
+					INNER JOIN MW_APRESENTACAO A ON A.ID_APRESENTACAO = IPV.ID_APRESENTACAO AND A.ID_EVENTO = TC.ID_EVENTO
+					WHERE PV.ID_PEDIDO_VENDA = ?
+					ORDER BY TC.DT_INICIO_VIGENCIA DESC";
+	$rsServicos = executeSQL($mainConnection, $queryServicos, array($pedido_id), true);
+
+	$itensPedido = array();
+	$i = -1;
+	while ($itens = fetchResult($result)) {
+	    $i++;
+
+	    if ($i == 0) {
+	        if ($rsServicos['IN_TAXA_POR_PEDIDO'] == 'S') {
+	            $valorConveniencia = obterValorServico($itens['ID_APRESENTACAO_BILHETE'], true, $pedido_id);
+
+	            $itensPedido[$i]['descricao_item'] = 'Serviço';
+	            $itensPedido[$i]['valor_item'] = $valorConveniencia;
+
+	            $valorConveniencia = 0;
+	            $i++;
+	        } else {
+	            $valorConveniencia = obterValorServico($itens['ID_APRESENTACAO_BILHETE'], false, $pedido_id);
+	        }
+	    } else {
+	        $valorConveniencia = obterValorServico($itens['ID_APRESENTACAO_BILHETE'], false, $pedido_id);
+	    }
+
+	    $evento_info = getEvento($itens['ID_EVENTO']);
+
+	    $itensPedido[$i]['descricao_item']['evento'] = utf8_encode($itens['DS_EVENTO']);
+	    $itensPedido[$i]['descricao_item']['data'] = $itens['DT_APRESENTACAO'];
+	    $itensPedido[$i]['descricao_item']['hora'] = $itens['HR_APRESENTACAO'];
+	    $itensPedido[$i]['descricao_item']['teatro'] = utf8_encode($evento_info['nome_teatro']);
+	    $itensPedido[$i]['descricao_item']['setor'] = utf8_encode($itens['DS_SETOR']);
+	    $itensPedido[$i]['descricao_item']['cadeira'] = utf8_encode($itens['DS_CADEIRA']);
+	    $itensPedido[$i]['descricao_item']['bilhete'] = utf8_encode($itens['DS_TIPO_BILHETE']);
+
+	    $itensPedido[$i]['valor_item'] = ($itens['VL_LIQUIDO_INGRESSO'] + $valorConveniencia);
+	    $itensPedido[$i]['id_base'] = $itens['ID_BASE'];
+	    $itensPedido[$i]['CodApresentacao'] = $itens['CodApresentacao'];
+	    $itensPedido[$i]['CodVenda'] = $itens['CodVenda'];
+	}
+} else {
+	include('errorMail.php');
+}
+
+$json = json_encode(array('descricao' => '7. envio do email de sucesso - pedido ' . $pedido_id));
+include('logiPagareChamada.php');
+
+include('successMail.php');

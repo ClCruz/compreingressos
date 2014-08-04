17486b2f1804d6bf620774c66cb6492468d3d1fe
Jefferson Ferreira <jefferson.ferreira@intuiti.com.br>
2014-08-04 19:53:27 +0000

    melhorado a documentação do processo de atualização dos bins do itau
    git-svn-id: https://portal.cc.com.br:8084/svn/projetos@2145 236fd033-a1ae-4692-b96d-e3c3cfb81559

--- Procedures/Incluir novos BINS.sql
+++ Procedures/Incluir novos BINS.sql
@@ -1,19 +1,17 @@
 --Jefferson - 17/01/2012
 --Processo para incluir novos BINS na base de acordo com relação enviada pela Compreingressos
---1o. verificar se o patrocinador já está cadastrado
+--1o. Carregar a planilha Excel para a tabela TEMP
+
+--2o. verificar se o patrocinador já está cadastrado
 select * from mw_patrocinador 
 
---2o. verificar se a bandeira do cartão já está cadastrada
+--3o. verificar se a bandeira do cartão já está cadastrada
 select * from mw_bandeira_cartao 
-insert into mw_bandeira_cartao values ('N/I')
---3o. incluir o novo registro de acordo com a relação recebida da Compreingressos
+--insert into mw_bandeira_cartao values ('N/I')
 
---exemplo: insert into mw_cartao_patrocinado 
---        (id_patrocinador, ds_cartao_patrocinado, cd_bin, id_bandeira_cartao)
---         values (1,'TIM ITAUCARD GOLD MASTERCARD','526892',2)
-select * from mw_cartao_patrocinado
-
-commit
+--4o. incluir o novo registro de acordo com a relação recebida da Compreingressos
+--Se os cadastros de bandeira e patrocinador estiverem OK, basta rodar o insert abaixo para atualizar o sistema e
+--em seguida rodar o JOB "Cria BINS para novos eventos" para atualizar a mw_evento_patrocinado.
 insert into mw_cartao_patrocinado
 select 1, left([Descrição do Produto],50), [BIN Itaú], b.id_bandeira_cartao
 from temp t
@@ -24,7 +22,13 @@ where not exists (select 1 from mw_cartao_patrocinado c
 					where [BIN Itaú] =cd_bin)
 					and [BIN Itaú] is not null
 
---4o. incluir o novo registro na mw_evento_patrocinado, relacionando o mw_evento e a mw_cartao_patrocinado
+--exemplo: insert into mw_cartao_patrocinado 
+--        (id_patrocinador, ds_cartao_patrocinado, cd_bin, id_bandeira_cartao)
+--         values (1,'TIM ITAUCARD GOLD MASTERCARD','526892',2)
+--select * from mw_cartao_patrocinado
+
+--5o. incluir o novo registro na mw_evento_patrocinado, relacionando o mw_evento e a mw_cartao_patrocinado
+--será necessário rodar.
 --Exemplo:insert into mw_evento_patrocinado 
 --        (id_cartao_patrocinado, id_base, codpeca, dt_inicio, dt_fim)
 --         select '651', id_base, codpeca, '2012-01-17 00:00', '2013-03-31 00:00' from mw_evento

397fa68dfc3cc57d701e268316a8ab0c883b0a06
Gabriel Monteiro <gabriel.monteiro@intuiti.com.br>
2014-08-11 13:55:37 +0000

    git-svn-id: https://portal.cc.com.br:8084/svn/projetos@2156 236fd033-a1ae-4692-b96d-e3c3cfb81559

--- Tickets/448/admin/actions/controleEntrada.php
+++ Tickets/448/admin/actions/controleEntrada.php
@@ -1,230 +1,274 @@
 <?php
-require_once('../settings/functions.php');
-
-$mainConnection = mainConnection();
-
-session_start();
+require_once('../settings/settings.php');
 
 if (acessoPermitido($mainConnection, $_SESSION['admin'], 320, true)) {
 
-    $pagina = basename(__FILE__);
+	if (isset($_POST['codigo'])) { /*------------ CHECAR BILHETE ------------*/
 
-    if (isset($_GET['action']) or isset($_POST['codigo'])) {
-        
-        require('actions/'.$pagina);
 
-    } else {
-?>
 
-<html>
-    <script>
-        $(document).ready(function(){
-            var pagina = '<?php echo $pagina; ?>',
-                $table_leitura = $('#table_leitura'),
-                $play_stop = $('#play_stop'),
-                $table_filtro = $('#table_filtro'),
-                $dados = $('#dados'),
-                $resultado_leitura = $('#resultado_leitura'),
-                $codigo = $('#codigo'),
-                $document = $(document),
-                $cboTeatro = $('#cboTeatro'),
-                $cboPeca = $('#cboPeca'),
-                $cboApresentacao = $('#cboApresentacao'),
-                $cboHorario = $('#cboHorario');
 
-            $('.button, [type="button"]').button();
+		// TEATRO MUNICIPAL
+		if ($_POST['cboTeatro'] == 139 || $is_teste) {
+			$conn = getConnection($_POST['cboTeatro']);
 
-            $play_stop.on('click', function(){
+			$query = "SELECT DTHRENTRADA, [Data do Evento], [Horario do Evento] FROM TABCODBARRATEMP WHERE CODIGOBARRAS = ?";
+			$params = array($_POST['codigo']);
+			$result = executeSQL($conn, $query, $params);
 
-                if ($cboTeatro.val() == '' || $cboPeca.val() == '' || $cboApresentacao.val() == '' || $cboHorario.val() == '') {
-                    $.dialog({
-                            title: 'Alerta...',
-                            text: 'Preencha todas as informações antes de iniciar a leitura.'
-                        });
-                    return false;
-                }
+			if (hasRows($result)) {
+				$rs = fetchResult($result);
 
-                $codigo.val('');
-                $resultado_leitura.removeClass('sucesso falha').html('');
+				// data confere?
+				if ($_POST['cboApresentacao'] != $rs['Data do Evento']->format("Ymd")) {
+					echo json_encode(array(
+						'class' => 'falha',
+						'mensagem' => 'Data do ingresso inválida para a apresentação.<br />Ingresso válido para: ' . $rs['Data do Evento']->format("d/m/Y") .' '. $rs['Horario do Evento']->format("H:i")
+					));
+					die();
+				}
 
-                if ($table_leitura.is(':hidden')) {
-                    $table_filtro.find('select').prop('disabled', true);
+				// hora confere?
+				if ($_POST['cboHorario'] != $rs['Horario do Evento']->format("H:i")) {
+					echo json_encode(array(
+						'class' => 'falha',
+						'mensagem' => 'Este ingresso pertence a outro horário.<br />Ingresso válido para: ' . $rs['Data do Evento']->format("d/m/Y") .' '. $rs['Horario do Evento']->format("H:i")
+					));
+					die();
+				}
 
-                    $table_leitura.show();
-                    $play_stop.val('Parar Leitura');
+				if ($rs['DTHRENTRADA'] == null) {
+					executeSQL($conn, 'UPDATE TABCODBARRATEMP SET DTHRENTRADA = GETDATE() WHERE CODIGOBARRAS = ?', $params);
 
-                    $document.on('click blur focus', function(){
-                        $codigo.focus().select();
-                    });
+					$retorno = array('class' => 'sucesso', 'mensagem' => 'Acesso autorizado.');
+				} else {
+					$retorno = array('class' => 'falha', 'mensagem' => 'Este ingresso já foi processado em ' .$rs['DTHRENTRADA']->format("d/m/Y H:i:s"). '.<br />Acesso não permitido.');
+				}
 
-                    $codigo.val('').trigger('focus');
-                } else {
-                    $table_filtro.find('select').prop('disabled', false);
+				echo json_encode($retorno);
+				die();
+			}
 
-                    $table_leitura.hide();
-                    $play_stop.val('Iniciar Leitura');
+			$query = "SELECT DTHRENTRADA, DATA_EVENTO, HORA_EVENTO FROM TABCODBARRATEMPINGFAC WHERE CODIGO_BARRAS = ? OR CODIGO_BARRAS = 'FF000' + ?";
+			$params = array($_POST['codigo'], $_POST['codigo']);
+			$result = executeSQL($conn, $query, $params);
 
-                    $document.off('click blur focus');
-                }
-            });
+			if (hasRows($result)) {
+				$rs = fetchResult($result);
 
-            $dados.on('submit', function(e){
-                e.preventDefault();
+				// data confere?
+				if ($_POST['cboApresentacao'] != $rs['DATA_EVENTO']->format("Ymd")) {
+					echo json_encode(array(
+						'class' => 'falha',
+						'mensagem' => 'Data do ingresso inválida para a apresentação.<br />Ingresso válido para: ' . $rs['DATA_EVENTO']->format("d/m/Y") .' '. $rs['HORA_EVENTO']->format("H:i")
+					));
+					die();
+				}
 
-                $codigo.prop('readonly', true);
-                $disabled_fields = $dados.find(':disabled').prop('disabled', false);
+				// hora confere?
+				if ($_POST['cboHorario'] != $rs['HORA_EVENTO']->format("H:i")) {
+					echo json_encode(array(
+						'class' => 'falha',
+						'mensagem' => 'Este ingresso pertence a outro horário.<br />Ingresso válido para: ' . $rs['DATA_EVENTO']->format("d/m/Y") .' '. $rs['HORA_EVENTO']->format("H:i")
+					));
+					die();
+				}
 
-                $resultado_leitura
-                    .removeClass('sucesso falha')
-                    .html('<img src="../images/catraca_loading.gif" />');
+				if ($rs['DTHRENTRADA'] == null) {
+					executeSQL($conn, "UPDATE TABCODBARRATEMPINGFAC SET DTHRENTRADA = GETDATE() WHERE CODIGO_BARRAS = ? OR CODIGO_BARRAS = 'FF000' + ?", $params);
 
-                $.ajax({
-                    url: pagina,
-                    type: 'POST',
-                    data: $dados.serialize(),
-                    dataType: "json"
-                }).done(function(data){
-                    $resultado_leitura
-                        .addClass(data.class)
-                        .html(data.mensagem);
-                }).fail(function(){
-                    $resultado_leitura
-                        .addClass('falha')
-                        .html('Falha na conexão.<br /><br />Favor tentar novamente.');
-                }).always(function(){
-                    $codigo.focus().select();
-                });
+					$retorno = array('class' => 'sucesso', 'mensagem' => 'Acesso autorizado.');
+				} else {
+					$retorno = array('class' => 'falha', 'mensagem' => 'Este ingresso já foi processado em ' .$rs['DTHRENTRADA']->format("d/m/Y H:i:s"). '.<br />Acesso não permitido.');
+				}
 
-                $disabled_fields.prop('disabled', true);
-                $codigo.prop('readonly', false);
-            });
+				echo json_encode($retorno);
+				die();
+			}
+		}
 
-            $.ajax({
-                url: pagina + '?action=cboTeatro'
-            }).done(function(html){
-                $cboTeatro.html(html);
-            });
 
-            $cboTeatro.on('change', function(){
-                $.ajax({
-                    url: pagina + '?action=cboPeca&cboTeatro=' + $cboTeatro.val()
-                }).done(function(html){
-                    $cboPeca.html(html).trigger('change');
-                });
-            });
 
-            $cboPeca.on('change', function(){
-                $.ajax({
-                    url: pagina + '?action=cboApresentacao&cboTeatro=' + $cboTeatro.val() + '&cboPeca=' + $cboPeca.val()
-                }).done(function(html){
-                    $cboApresentacao.html(html).trigger('change');
-                });
-            });
 
-            $cboApresentacao.on('change', function(){
-                $.ajax({
-                    url: pagina + '?action=cboHorario&cboTeatro=' + $cboTeatro.val() + '&cboPeca=' + $cboPeca.val() + '&cboApresentacao=' + $cboApresentacao.val()
-                }).done(function(html){
-                    $cboHorario.html(html).trigger('change');
-                });
-            })
-        });
-    </script>
-    <head>
-        <style type="text/css">
-            #table_leitura {
-                display: none;
-            }
+		if (!is_numeric($_POST['codigo']) or strlen($_POST['codigo']) != 22) {
+			echo json_encode(array(
+				'class' => 'falha',
+				'mensagem' => 'Código inválido.'
+			));
+			die();
+		}
 
-            #codigo {
-                border: none;
-                border-bottom: 1px solid #000;
-                font-size: 30px;
-                line-height: 30px;
-                text-align: center;
-                width: 550px;
-            }
+		// data confere?
+		if (substr($_POST['cboApresentacao'], -4) != substr($_POST['codigo'], 6, 4)) {
+			echo json_encode(array(
+				'class' => 'falha',
+				'mensagem' => 'Data do ingresso inválida para a apresentação.<br />Ingresso válido para: ' . substr($_POST['codigo'], 8, 2) .'/'. substr($_POST['codigo'], 6, 2)
+			));
+			die();
+		}
 
-            #codigo::selection {
-                background: white;
-            }
+		// hora confere?
+		if (str_replace(':', '', $_POST['cboHorario']) != substr($_POST['codigo'], 10, 4)) {
+			echo json_encode(array(
+				'class' => 'falha',
+				'mensagem' => 'Este ingresso pertence a outro horário.<br />Ingresso válido para: ' . substr($_POST['codigo'], 10, 2) .':'. substr($_POST['codigo'], 12, 2)
+			));
+			die();
+		}
 
-            #codigo::-moz-selection {
-                background: white;
-            }
+		$conn = getConnection($_POST['cboTeatro']);
+		
+		// evento confere?
+		$query = "SELECT CODPECA FROM TABAPRESENTACAO WHERE CODAPRESENTACAO = ?";
+		$params = array(substr($_POST['codigo'], 0, 5));
+		$rs = executeSQL($conn, $query, $params, true);
 
-            #resultado_leitura {
-                font-size: 50px;
-                padding: 20px;
-            }
+		if ($_POST['cboPeca'] != $rs['CODPECA']) {
+			echo json_encode(array(
+				'class' => 'falha',
+				'mensagem' => 'Este ingresso pertence a outro evento.'
+			));
+			die();
+		}
+		
+		$query = "SELECT B.NUMSEQ, B.CODAPRESENTACAO, B.INDICE, B.STATUSINGRESSO, B.DATHRENTRADA
+					FROM TABCONTROLESEQVENDA A
+					INNER JOIN TABCONTROLESEQVENDA B ON B.CODAPRESENTACAO = A.CODAPRESENTACAO AND B.INDICE = A.INDICE AND B.STATUSINGRESSO = A.STATUSINGRESSO
+					WHERE A.CODBAR = ?";
+		$params = array($_POST['codigo']);
+		$result = executeSQL($conn, $query, $params);
+		
+		if (hasRows($result)) {
+			// pode retornar 2 linhas no caso de complemento de ingressos, mas como sao o mesmo ingresso podem ser tratados como 1 so
+			while ($rs = fetchResult($result)) {
+				if ($rs['STATUSINGRESSO'] == 'L') {
+					$query = "UPDATE TABCONTROLESEQVENDA SET
+								DATHRENTRADA = GETDATE(),
+								STATUSINGRESSO = 'U'
+								WHERE NUMSEQ = ?
+								AND CODAPRESENTACAO =?
+								AND INDICE = ?";
+					$params = array($rs['NUMSEQ'], $rs['CODAPRESENTACAO'], $rs['INDICE']);
+					executeSQL($conn, $query, $params);
 
-            .sucesso {
-                color: darkgreen;
-                background-color: lightgreen;
-                border: 5px solid darkgreen;
-            }
+					$retorno = array('class' => 'sucesso', 'mensagem' => 'Acesso autorizado.');
 
-            .falha {
-                color: darkred;
-                background-color: lightpink;
-                border: 5px solid darkred;
-            }
-        </style>
-</head>
-<body>
-    <h2>Controle de Entrada</h2>
-    <form id="dados" action="" method="POST">
-        <table id="table_filtro">
-            <tr>
-                <td>
-                    <strong>Local:</strong><br>
-                    <select name="cboTeatro" id="cboTeatro"><option value="">Carregando...</option></select>
-                </td>
-                <td colspan="2">
-                    <strong>Evento:</strong><br>
-                    <select name="cboPeca" id="cboPeca"><option value="">Selecione um Local...</option></select>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <br>
-                    <strong>Apresenta&ccedil;&atilde;o:</strong><br>
-                    <select name="cboApresentacao" id="cboApresentacao"><option value="">Selecione um Evento...</option></select>
-                </td>
-                <td>
-                    <br>
-                    <strong>Hor&aacute;rio:</strong><br>
-                    <select name="cboHorario" id="cboHorario"><option value="">Selecione uma Apresentação...</option></select>
-                </td>
-                <td style="vertical-align: bottom;">
-                    <input id="play_stop" type="button" value="Iniciar Leitura" />
-                </td>
-            </tr>
-        </table>
+				} elseif ($rs['STATUSINGRESSO'] == 'U') {
 
-        <table id="table_leitura">
-            <tr>
-                <td>
-                    <h2>Leitura</h2>
-                </td>
-            </tr>
+					$retorno = array('class' => 'falha', 'mensagem' => 'Este ingresso já foi processado em ' .$rs['DATHRENTRADA']->format("d/m/Y H:i:s"). '.<br />Acesso não permitido.');
 
-            <tr>
-                <td align="center">
-                    <input type="text" name="codigo" id="codigo" />
-                </td>
-            </tr>
+				} elseif ($rs['STATUSINGRESSO'] == 'E') {
 
-            <tr>
-                <td align="center">
-                    <div id="resultado_leitura"><img src="../images/catraca_loading.gif" /></div>
-                </td>
-            </tr>
-        </table>
-    </form>
-</BODY>
-</html>
-<?php
-    }
+					$retorno = array('class' => 'falha', 'mensagem' => 'Ingresso estornado.<br />Acesso não permitido.');
+
+				} else {
+
+					$retorno = array('class' => 'falha', 'mensagem' => 'Ingresso com status desconhecido.<br />Acesso não permitido.');
+				}
+			}
+		} else {
+			$retorno = array('class' => 'falha', 'mensagem' => 'Código do ingresso não existe.<br />Acesso não permitido.');
+		}
+
+		echo json_encode($retorno);
+		
+	} elseif ($_GET['action'] == 'cboTeatro') {
+
+		$query = "SELECT DISTINCT B.ID_BASE, B.DS_NOME_TEATRO
+					FROM MW_BASE B
+					INNER JOIN MW_ACESSO_CONCEDIDO AC ON AC.ID_BASE = B.ID_BASE
+					WHERE AC.ID_USUARIO = ? AND B.IN_ATIVO = '1'
+					ORDER BY B.DS_NOME_TEATRO";
+		$result = executeSQL($mainConnection, $query, array($_SESSION['admin']));
+
+		$combo = '<option value="">Selecione...</option>';
+        while ($rs = fetchResult($result)) {
+            $combo .= '<option value="' . $rs['ID_BASE'] . '"' . (($selected == $rs['ID_BASE']) ? ' selected' : '') . '>' . utf8_encode($rs['DS_NOME_TEATRO']) . '</option>';
+        }
+
+        echo $combo;
+        die();
+
+	} elseif ($_GET['action'] == 'cboPeca' and isset($_GET['cboTeatro'])) {
+
+		$conn = getConnection($_GET['cboTeatro']);
+
+		$query = "EXEC SP_PEC_CON009;8 ?, ?";
+		$params = array($_SESSION['admin'], $_GET['cboTeatro']);
+		$result = executeSQL($conn, $query, $params);
+
+		$html = '<option value="">Selecione...</option>';
+
+		while($rs = fetchResult($result)){
+			$html .= '<option value="'. $rs["CodPeca"] .'">'. utf8_encode($rs["nomPeca"]) .'</option>';	
+		}
+
+		echo $html;
+		die();
+
+	} elseif ($_GET['action'] == 'cboApresentacao' and isset($_GET['cboTeatro']) and isset($_GET['cboPeca'])) {
+
+		$conn = getConnection($_GET['cboTeatro']);
+
+		$query = "SELECT tbAp.DatApresentacao
+		            from tabApresentacao tbAp (nolock)
+		            inner join tabPeca tbPc (nolock)
+		                        on        tbPc.CodPeca = tbAp.CodPeca
+		            inner join ci_middleway..mw_acesso_concedido iac (nolock)
+		                        on                    iac.id_base = ?
+										and			  iac.id_usuario = ?
+										and			  iac.CodPeca = tbAp.CodPeca
+		            where               tbPc.CodPeca = ?
+		            -- comentar para homologacao
+					--AND CONVERT(DATETIME, CONVERT(VARCHAR(8), TBAP.DATAPRESENTACAO, 112) + ' ' + TBAP.HORSESSAO) >= CONVERT(DATETIME, CONVERT(VARCHAR(8), DATEADD(DAY, -1, GETDATE()), 112) + ' 22:00')
+					--AND TBAP.DATAPRESENTACAO <= GETDATE()
+					----------------------------
+		            group by tbAp.DatApresentacao
+		            order by tbAp.DatApresentacao";
+		$params = array($_GET['cboTeatro'], $_SESSION['admin'], $_GET['cboPeca']);
+		$result = executeSQL($conn, $query, $params);
+
+		$html = '<option value="">Selecione...</option>';
+		
+		while($rs = fetchResult($result)){
+			$html .= '<option value="'. $rs["DatApresentacao"]->format("Ymd") .'">'. $rs["DatApresentacao"]->format("d/m/Y") .'</option>';	
+		}
+		
+		echo $html;
+		die();
+
+	} elseif ($_GET['action'] == 'cboHorario' and isset($_GET['cboTeatro']) and isset($_GET['cboPeca']) and isset($_GET['cboApresentacao'])) {
+
+		$conn = getConnection($_GET['cboTeatro']);
+
+		$query = "SELECT HorSessao
+		            from tabApresentacao tbAp (nolock)
+		            inner join tabPeca tbPc (nolock)
+		                        on        tbPc.CodPeca = tbAp.CodPeca
+		            inner join ci_middleway..mw_acesso_concedido iac (nolock)
+		                        on                    iac.id_base = ?
+										and			  iac.id_usuario = ?
+										and			  iac.CodPeca = tbAp.CodPeca
+		            where       tbPc.CodPeca = ?
+		            -- comentar para homologacao
+		            --AND CONVERT(DATETIME, CONVERT(VARCHAR(8), TBAP.DATAPRESENTACAO, 112) + ' ' + TBAP.HORSESSAO) >= CONVERT(DATETIME, CONVERT(VARCHAR(8), DATEADD(DAY, -1, GETDATE()), 112) + ' 22:00')
+					----------------------------
+		            AND TBAP.DATAPRESENTACAO = CONVERT(DATETIME, ?, 112)
+		            group by tbAp.HorSessao
+		            order by tbAp.HorSessao";
+		$params = array($_GET['cboTeatro'], $_SESSION['admin'], $_GET['cboPeca'], $_GET['cboApresentacao']);
+		$result = executeSQL($conn, $query, $params);
+
+		$html = '<option value="">Selecione...</option>';
+
+		while($rs = fetchResult($result)){
+			$html .= '<option value="'. $rs["HorSessao"] .'">'. $rs["HorSessao"] .'</option>';	
+		}
+
+		echo $html;
+		die();
+
+	}
+
 }
 ?>

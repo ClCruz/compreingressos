2eb12973f2585c435658dee32c1a85684a7b3220
Gabriel Monteiro <gabriel.monteiro@intuiti.com.br>
2014-10-13 12:05:45 +0000

    #507
    git-svn-id: https://portal.cc.com.br:8084/svn/projetos@2251 236fd033-a1ae-4692-b96d-e3c3cfb81559

--- Site/admin/actions/envioIngressos.php
+++ Site/admin/actions/envioIngressos.php
@@ -10,6 +10,12 @@ if ($_GET['action'] == 'update' and isset($_GET['codestado'])) { /*------------ 
 	$params = array($_POST['qtdhoras'], $_GET['codestado']);
 	
 	if (executeSQL($mainConnection, $query, $params)) {
+        $log = new Log($_SESSION['admin']);
+        $log->__set('funcionalidade', 'Envio de Ingressos');
+        $log->__set('parametros', $params);
+        $log->__set('log', $query);
+        $log->save($mainConnection);
+        
 		$retorno = 'true?codestado='.$_GET['codestado'];
 	} else {
 		$retorno = sqlErrors();

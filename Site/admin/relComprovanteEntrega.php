c8322ea7b5bc2539d9a7ad45f39ae0deeb66391b
Edicarlos Barbosa <edicarlos.barbosa@intuiti.com.br>
2014-12-30 12:45:56 +0000

    Corrigido acentos na página e removido apresentações filhas da lista de comprovantes.
    git-svn-id: https://portal.cc.com.br:8084/svn/projetos@2406 236fd033-a1ae-4692-b96d-e3c3cfb81559

--- Site/admin/relComprovanteEntrega.php
+++ Site/admin/relComprovanteEntrega.php
@@ -40,8 +40,7 @@ inner join mw_evento e on e.id_evento = a.id_evento
 inner join mw_local_evento le on le.id_local_evento = e.id_local_evento
 WHERE CODVENDA = ?
 	AND PV.IN_RETIRA_ENTREGA = 'E'
-	AND PV.IN_SITUACAO_DESPACHO != 'E'
-        AND PV.ID_PEDIDO_PAI IS NULL";
+	AND PV.IN_SITUACAO_DESPACHO != 'E'";
 
 //Consultar itens do pedido
 $sqlItens = "SELECT
@@ -62,7 +61,6 @@ LEFT JOIN MW_APRESENTACAO_BILHETE AB ON
 WHERE CODVENDA = ?
 	AND PV.IN_RETIRA_ENTREGA = 'E'
 	AND PV.IN_SITUACAO_DESPACHO != 'E'
-        AND PV.ID_PEDIDO_PAI IS NULL
 GROUP BY
 	IPV.VL_UNITARIO,
 	AB.CODTIPBILHETE,

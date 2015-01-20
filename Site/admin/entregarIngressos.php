a98c694c82067f20b0bb78a8dfcea5b1c77c471e
Gabriel Monteiro <gabriel.monteiro@intuiti.com.br>
2015-01-20 17:33:35 +0000

    correcao na query principal
    git-svn-id: https://portal.cc.com.br:8084/svn/projetos@2423 236fd033-a1ae-4692-b96d-e3c3cfb81559

--- Site/admin/entregarIngressos.php
+++ Site/admin/entregarIngressos.php
@@ -18,7 +18,7 @@ if (acessoPermitido($mainConnection, $_SESSION['admin'], 216, true)) {
         if (isset($_GET["dt_inicial"]) && isset($_GET["dt_final"]) && isset($_GET["situacao"]) && isset($_GET["nm_cliente"]) && isset($_GET["cd_cpf"]) && isset($_GET["num_pedido"])) {
 
             $where = "WHERE CONVERT(DATETIME,CONVERT(CHAR(8), PV.DT_PEDIDO_VENDA, 112)) BETWEEN CONVERT(DATETIME, ?, 103) AND CONVERT(DATETIME, ?, 103) 
-                        AND PV.IN_SITUACAO = 'F' AND PV.IN_RETIRA_ENTREGA = 'E'";
+                        AND PV.IN_SITUACAO = 'F' AND PV.IN_RETIRA_ENTREGA = 'E' AND PV.ID_PEDIDO_PAI IS NULL ";
 
             $params = array($_GET["dt_inicial"], $_GET["dt_final"]);
 
@@ -31,7 +31,7 @@ if (acessoPermitido($mainConnection, $_SESSION['admin'], 216, true)) {
                     C.DS_SOBRENOME,
                     SUM(IPV.VL_UNITARIO) AS TOTAL_UNIT,
                     PV.IN_SITUACAO,
-                    ROW_NUMBER() OVER(ORDER BY PV.ID_PEDIDO_VENDA DESC) AS 'LINHA',
+                    ROW_NUMBER() OVER(ORDER BY PV.ID_PEDIDO_VENDA) AS 'LINHA',
                     COUNT(1) AS QUANTIDADE,
                     PV.IN_RETIRA_ENTREGA,
                     C.DS_DDD_TELEFONE,

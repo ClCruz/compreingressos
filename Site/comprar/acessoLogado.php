67e82bd37783c42aa11683e60f7384680c81dc45
Gabriel Monteiro <gabriel.monteiro@intuiti.com.br>
2015-05-20 20:12:07 +0000

    #598
    git-svn-id: https://portal.cc.com.br:8084/svn/projetos@2492 236fd033-a1ae-4692-b96d-e3c3cfb81559

--- Site/comprar/acessoLogado.php
+++ Site/comprar/acessoLogado.php
@@ -1,11 +1,16 @@
 <?php
 require_once('../settings/settings.php');
 require_once('../settings/functions.php');
+require_once('../settings/Cypher.class.php');
+
 session_start();
 
 //ACESSO PERMITIDO APENAS PARA CLIENTES LOGADOS
 if (isset($_SESSION['user'])) {
-	setcookie('user', true, $cookieExpireTime);
+	$cipher = new Cipher('1ngr3ss0s');
+	$encryptedtext = $cipher->encrypt($_SESSION['user']);
+
+	setcookie('user', $encryptedtext, $cookieExpireTime);
 } else {
 	header("Location: login.php?redirect=" . urlencode(getCurrentUrl()));
 }

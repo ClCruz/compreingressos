fecb5643c940a897bad7098fc2304ef02505127c
Gabriel Monteiro <gabriel.monteiro@intuiti.com.br>
2015-06-01 16:40:44 +0000

    #605
    git-svn-id: https://portal.cc.com.br:8084/svn/projetos@2501 236fd033-a1ae-4692-b96d-e3c3cfb81559

--- Tickets/605/admin/actions/mapaPlateia.php
+++ Tickets/605/admin/actions/mapaPlateia.php
@@ -1,152 +1,183 @@
 <?php
-require_once('../settings/functions.php');
-include('../settings/Log.class.php');
-$mainConnection = mainConnection();
-session_start();
 
 if (acessoPermitido($mainConnection, $_SESSION['admin'], 11, true)) {
 
-  $pagina = basename(__FILE__);
+  if ($_GET['action'] == 'load') { /* ------------ LOAD ------------ */
+    $conn = getConnection($_POST['teatro']);
 
-  if (isset($_GET['action'])) {
+    $query = 'SELECT NOMEIMAGEMSITE, ALTURASITE, LARGURASITE, TAMANHOLUGAR, FOTOIMAGEMSITE
+              FROM TABSALA WHERE CODSALA = ?';
+    $params = array($_POST['sala']);
+    $rs = executeSQL($conn, $query, $params, true);
 
-    require('actions/' . $pagina);
-  } else {
-?>
-    <link rel="stylesheet" href="../stylesheets/annotations.css"/>
-    <link rel="stylesheet" href="../stylesheets/ajustes.css"/>
-    <link rel="stylesheet" href="../stylesheets/plateiaEdicao.css"/>
-    <link rel="stylesheet" href="../javascripts/uploadify/uploadify.css"/>
+    if ($rs['FOTOIMAGEMSITE'] != '') {
+      $imagem = $rs['FOTOIMAGEMSITE'];
+    }
+    if ($rs[1] != '') {
+      $yScale = $rs[1];
+    }
+    if ($rs[2] != '') {
+      $xScale = $rs[2];
+    }
+    if ($rs[3] != '') {
+      $size = $rs[3];
+    }
 
-    <script type="text/javascript" src="../javascripts/jquery.utils.js"></script>
-    <script type="text/javascript" src="../javascripts/jquery.annotate.js"></script>
-    <script type="text/javascript" src="../javascripts/plateiaEdicao.js"></script>
-    <script type="text/javascript" src="../javascripts/uploadify/swfobject.js"></script>
-    <script type="text/javascript" src="../javascripts/uploadify/jquery.uploadify.v2.1.0.min.js"></script>
-    <h2>Mapeamento de Plateia</h2>
-    <div id="containerDados">
-      <form id="dados" name="dados" method="post">
-        <table border="0" cellspacing="0" cellpadding="0">
-          <tr>
-            <td><h3>Local</h3></td>
-          </tr>
-          <tr>
-            <td><?php echo comboTeatro('teatroID'); ?></td>
-          </tr>
-          <tr>
-            <td><h3>Sala</h3></td>
-          </tr>
-          <tr>
-            <td><span id="celSala"><select><option>Selecione um teatro...</option></select></span></td>
-          </tr>
-          <tr>
-            <td><h3>Espa&ccedil;amento entre lugares:</h3></td>
-          </tr>
-          <tr>
-            <td>
-    					Horizontal:
-              <span style="float:left">(+)</span><span style="float:right">(-)</span>
-              <div id="xMargin"></div>
-            </td>
-          </tr>
-          <tr>
-            <td>
-    					Vertical:
-              <span style="float:left">(+)</span><span style="float:right">(-)</span>
-              <div id="yMargin"></div>
-            </td>
-          </tr>
-          <tr>
-            <td align="center" valign="middle">
-              <input type="button" id="resetEvento" class="button" value="Recalcular" />
-            </td>
-          </tr>
-        </table>
-        <table border="0" cellspacing="0" cellpadding="0">
-          <tr>
-            <td><h3>Tamanho do Lugar:</h3></td>
-          </tr>
-          <tr>
-            <td>
-              Tamanho:<input class="readonly" type="text" id="Size" value="10px" readonly /> <a href="#" id="sizeReset">reset</a>
-              <span style="float:left">(-)</span><span style="float:right">(+)</span>
-              <div id="ScaleSize"></div>
-            </td>
-          </tr>
-          <tr>
-            <td><h3>Visão do Lugar (foto):</h3></td>
-          </tr>
-          <tr>
-            <td>
-              <div id="lista_fotos"></div>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <div id="areaUploadFotos" style="width:300px; height:16px;">
-                Enviar fotos panorâmicas dos lugares para o servidor<br/>
-                <div style="width:300px; height:16px; position:absolute; top:auto; z-index:1;"><input type="button" class="button" value="Enviar Fotos" /></div>
-                <div style="width:300px; height:16px; position:absolute; top:auto; z-index:100; opacity:0; filter:Alpha(Opacity=0);"><input style="width:300px;" type="file" name="fotos" id="fotos" /></div>
-              </div>
-              &nbsp;
-            </td>
-          </tr>
-          <tr>
-            <td><div id="uploadifyQueue3" class="uploadifyQueue"></div>&nbsp;</td>
-          </tr>
-        </table>
-        <table border="0" cellspacing="0" cellpadding="0">
-          <tr>
-            <td><h3>Tamanho da imagem de fundo:</h3></td>
-          </tr>
-          <tr>
-            <td>
-    					Horizontal:<input class="readonly" type="text" id="xScaleAmount" value="510px" readonly /> <a href="#" id="xReset">reset</a>
-              <span style="float:left">(-)</span><span style="float:right">(+)</span>
-              <div id="xScale"></div>
-            </td>
-          </tr>
-          <tr>
-            <td>
-    					Vertical:<input class="readonly" type="text" id="yScaleAmount" value="630px" readonly /> <a href="#" id="yReset">reset</a>
-              <span style="float:left">(-)</span><span style="float:right">(+)</span>
-              <div id="yScale"></div>
-            </td>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td align="left" valign="middle">
-              <input type="button" id="removerImagem" class="button" value="Remover Imagem" style="display:inline-block" />
-              <div style="width:97px; height:16px; display:inline-block">
-                <div style="width:97px; height:16px; position:absolute; top:auto; z-index:1;"><input type="button" id="trocarImagem" class="button" value="Trocar Imagem" /></div>
-                <div style="width:97px; height:16px; position:absolute; top:auto; z-index:100; opacity:0; filter:Alpha(Opacity=0);"><input type="file" name="background" id="background" /></div>
-              </div>
-              <div id="uploadifyQueue2" class="uploadifyQueue"></div>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td align="center" valign="middle">
-              <input type="button" id="carregaEvento" class="button" value="Carregar" />
-              <input type="button" id="salvarEvento" class="button" value="Salvar" />
-            </td>
-          </tr>
-        </table>
-      </form>
-    </div>
-    <div id="mapa_de_plateia" class="edicao">
-      <img src="../images/palco.png" width="630" height="500">
-    </div>
+    $query = "SELECT MAX(POSX) MAXX, MAX(POSY) MAXY, MAX(POSXSITE) MAXXSITE, 
+              MAX(POSYSITE) MAXYSITE FROM TABSALDETALHE
+              WHERE CODSALA = ? AND TIPOBJETO = 'C'";
+    $params = array($_POST['sala']);
+    $rs = executeSQL($conn, $query, $params, true);
 
-    <div id="dialog-confirm">
-      <span class="img"></span>
-      <span class="text">Deseja aplicar a imagem aos itens selecionados?</span>
-    </div>
+    $query = 'SELECT S.INDICE, S.NOMOBJETO, S.CODSETOR, SE.NOMSETOR, S.IMGVISAOLUGAR, S.IMGVISAOLUGARFOTO, ';
 
-<?php
+    if ($rs['MAXXSITE'] == '' or $rs['MAXYSITE'] == '' or $_POST['reset']) {
+      $query .= '(((S.POSX * ?) / ?) + ?) POSXSITE, (((S.POSY * ?) / ?) + ?) POSYSITE';
+      $params = array(
+          1 - $_POST['xmargin'],
+          $rs['MAXX'],
+          $_POST['xmargin'],
+          1 - $_POST['ymargin'],
+          $rs['MAXY'],
+          $_POST['ymargin'],
+          $_POST['sala']
+      );
+    } else {
+      $query .= 'S.POSXSITE, S.POSYSITE';
+      $params = array($_POST['sala']);
+    }
+
+    $query .= ' FROM TABSALDETALHE S
+                INNER JOIN TABSETOR SE ON SE.CODSALA = S.CODSALA
+                AND SE.CODSETOR = S.CODSETOR
+                WHERE S.CODSALA = ? AND S.TIPOBJETO = \'C\'';
+
+    $result = executeSQL($conn, $query, $params);
+
+    $cadeiras = array();
+    $imagens = array();
+    
+    while ($rs = fetchResult($result)) {
+      
+      $rs['STATUS'] = (session_id() == $rs['ID_SESSION']) ? 'S' : $rs['STATUS'];
+      
+      $cadeira = array( 
+        "id" => $rs['INDICE'],
+        "name" => utf8_encode($rs['NOMOBJETO']),
+        "setor" => utf8_encode($rs['NOMSETOR']),
+        "codSetor" => $rs['CODSETOR'],
+        "x" => $rs['POSXSITE'],
+        "y" => $rs['POSYSITE']
+      );
+
+      if ($rs['IMGVISAOLUGARFOTO']) {
+        $img_index = md5($rs['IMGVISAOLUGARFOTO']);
+        $cadeira['img'] = $img_index;
+        $imagens[$img_index] = $rs['IMGVISAOLUGARFOTO'];
+      }
+
+      $cadeiras[] = $cadeira;
+    }
+
+    $data = array(
+      'cadeiras' => $cadeiras,
+      'imagens' => $imagens,
+      'imagem' => $imagem,
+      'xScale' => $xScale,
+      'yScale' => $yScale,
+      'size' => $size
+    );
+
+    header("Content-type: text/txt");
+
+    echo json_encode($data);
+  } else if ($_GET['action'] == 'save') {
+    //get upload path
+    require_once('../settings/settings.php');
+    //---------------
+
+    // checa se a imagem enviada é um caminho valido ou ja esta em base64
+    if ($_POST['image']) {
+      if (file_exists($uploadPath.$_POST['image'])) {
+        $imagem = getBase64ImgString($uploadPath.$_POST['image']);
+      } else {
+        $imagem = $_POST['image'];
+      }
+    } else {
+      $imagem = NULL;
+    }
+
+    $conn = getConnection($_POST['teatro']);
+
+    $query = 'UPDATE TABSALA SET FOTOIMAGEMSITE = ?,
+              LARGURASITE = ?,
+              ALTURASITE = ?,
+              TAMANHOLUGAR = ?
+              WHERE CODSALA = ?';
+    $params = array(
+        $imagem,
+        (isset($_POST['xScale'])) ? $_POST['xScale'] : '',
+        (isset($_POST['yScale'])) ? $_POST['yScale'] : '',
+        (isset($_POST['Size'])) ? $_POST['Size'] : '',
+        $_POST['sala']
+    );
+    executeSQL($conn, $query, $params);
+    
+    $log = new Log($_SESSION['admin']);
+    $log->__set('funcionalidade', 'Layout das Salas');
+    $log->__set('parametros', $params);
+    $log->__set('log', $query);
+    $log->save($mainConnection);
+
+    $obj = json_decode($_POST['obj']);
+
+    beginTransaction($conn);
+
+    foreach ($obj as $cadeira) {
+      $cadeira = get_object_vars($cadeira);
+
+      if ($cadeira['img'] == 'manter_imagem') {
+        $query = 'UPDATE TABSALDETALHE SET
+                  POSXSITE = ?,
+                  POSYSITE = ?
+                  WHERE CODSALA = ? AND INDICE = ?';
+        $params = array($cadeira['x'], $cadeira['y'], $_POST['sala'], $cadeira['id']);
+      } else {
+        $query = 'UPDATE TABSALDETALHE SET
+                  POSXSITE = ?,
+                  POSYSITE = ?,
+                  IMGVISAOLUGARFOTO = ?
+                  WHERE CODSALA = ? AND INDICE = ?';
+
+        $img_string = $cadeira['img'] ? getBase64ImgString($cadeira['img']) : NULL;
+
+        $params = array($cadeira['x'], $cadeira['y'], $img_string, $_POST['sala'], $cadeira['id']);
+      }
+
+      executeSQL($conn, $query, $params);
+    }
+
+    $erros = sqlErrors();
+
+    if (empty($erros)) {
+      commitTransaction($conn);
+      echo 'Dados salvos com sucesso!';
+    } else {
+      rollbackTransaction($conn);
+      echo sqlErrors('messsage');
+    }
+  } else if ($_GET['action'] == 'lista_fotos') {
+    require_once('../settings/settings.php');
+
+    $fotos_path = $uploadPath . 'fotos/' . $_REQUEST['teatro'] . '/';
+    
+    $files = array_diff(scandir($fotos_path), array('..', '.'));
+
+    echo "<span class='reset'>Sem Foto</span>";
+
+    foreach ($files as $file_name) {
+      echo "<img src='".$fotos_path.$file_name."' />";
+    }
   }
 }
 ?>

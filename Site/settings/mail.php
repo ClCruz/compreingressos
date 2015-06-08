d239baa0290b3b91773ba73db737288d5faf7e96
Gabriel Monteiro <gabriel.monteiro@intuiti.com.br>
2015-06-08 13:48:41 +0000

    #607
    git-svn-id: https://portal.cc.com.br:8084/svn/projetos@2506 236fd033-a1ae-4692-b96d-e3c3cfb81559

--- Site/settings/mail.php
+++ Site/settings/mail.php
@@ -10,27 +10,17 @@ function authSendEmail($from, $namefrom, $to, $nameto, $subject, $message, $copi
 	$mail->SetLanguage('br');
 	
 	$mail->IsSMTP();
-	$mail->Host = "smtp.compreingressos.com";
-	$mail->Host = "smtp.bra.terra.com.br";
-	// $mail->Host = "smtp.gmail.com";
-	$mail->Port = 587;
-	$mail->SMTPAuth = true;
-	$mail->Username = "lembrete@compreingressos.com";
-	$mail->Username = "gabriel.monteiro@cc.com.br";
-	// $mail->Username = "cecsuporte";
-	$mail->Password = "lembrete0015";
-	$mail->Password = "te14cx ";
+	$mail->Host = 'smtp-relay.gmail.com';//"smtp.compreingressos.com";
 
-	// somente gmail
-	// $mail->SMTPSecure = "tls";
+	$mail->SMTPSecure = "tls";
 	
-	$mail->From = $from;
+	$mail->Port = 587;
+	
+	// somente gmail
+	$mail->From = 'compreingressos@siscompre.com';//$from;
 	$mail->FromName = $namefrom;
 	
 	$mail->AddAddress($to, $nameto);
-	//$mail->AddAddress('e-mail@destino2.com.br');
-	//$mail->AddCC('copia@dominio.com.br', 'Copia');
-	//$mail->AddBCC('CopiaOculta@dominio.com.br', 'Copia Oculta');
 	
 	if (!empty($copiesTo)) {
 		foreach($copiesTo as $address) {
@@ -93,6 +83,90 @@ function authSendEmail_alternativo($from, $namefrom, $to, $nameto, $subject, $me
 	$mail = new PHPMailer();
 	
 	$mail->SMTPDebug = 0;
+	
+	$mail->SetLanguage('br');
+	
+	$mail->IsSMTP();
+	$mail->Host = 'smtp.gmail.com';//"smtp.compreingressos.com";
+
+	// somente gmail
+	$mail->SMTPSecure = "tls";
+	
+	$mail->Port = 587;
+	$mail->SMTPAuth = true;
+	$mail->Username = 'compreingressos@gmail.com';//"lembrete@compreingressos.com";
+	$mail->Password = 'cruz79513579';//"lembrete0015";
+	
+	// somente gmail
+	$mail->From = 'compreingressos@gmail.com';//$from;
+	$mail->FromName = $namefrom;
+	
+	$mail->AddAddress($to, $nameto);
+	//$mail->AddAddress('e-mail@destino2.com.br');
+	//$mail->AddCC('copia@dominio.com.br', 'Copia');
+	//$mail->AddBCC('CopiaOculta@dominio.com.br', 'Copia Oculta');
+	
+	if (!empty($copiesTo)) {
+		foreach($copiesTo as $address) {
+			$address = explode('=>', $address);
+			
+			$name = trim($address[0]);
+			$email = trim($address[1]);
+			
+			$mail->AddCC($email, $name);
+		}
+	}
+
+	if (!empty($hiddenCopiesTo)) {
+		foreach($hiddenCopiesTo as $address) {
+			$address = explode('=>', $address);
+			
+			$name = trim($address[0]);
+			$email = trim($address[1]);
+			
+			$mail->AddBCC($email, $name);
+		}
+	}
+
+	if (!empty($attachment)) {
+		foreach($attachment as $file) {
+			if ($file['cid']) {
+				$mail->AddEmbeddedImage($file['path'], $file['cid']);
+				//and on the <img> tag put src='cid:file_cid'
+			} else {
+				$mail->AddAttachment($file['path'], $file['new_name']);  
+			}
+		}
+	}
+	
+	$mail->IsHTML(true);
+	$mail->CharSet = $charset;
+	
+	$mail->Subject  = $subject;
+	$mail->Body = $message;
+	//$mail->AltBody = 'plain text';
+	
+	$enviado = $mail->Send();
+
+	$mail->ClearAllRecipients();
+	$mail->ClearAttachments();
+	if (!empty($attachment)) {
+		foreach($attachment as $file) {
+			unlink($file['path']);
+		}
+		limparImagesTemp();
+	}
+
+	$error = $mail->ErrorInfo;
+
+	return $enviado ? $enviado : authSendEmail_alternativo2($from, $namefrom, $to, $nameto, $subject, $message, $copiesTo, $hiddenCopiesTo, $charset, $attachment);
+}
+
+function authSendEmail_alternativo2($from, $namefrom, $to, $nameto, $subject, $message, $copiesTo = array(), $hiddenCopiesTo = array(), $charset = 'utf8', $attachment = array()) {
+	
+	$mail = new PHPMailer();
+	
+	$mail->SMTPDebug = 0;
 
 	$mail->SetLanguage('br');
 	
@@ -101,7 +175,7 @@ function authSendEmail_alternativo($from, $namefrom, $to, $nameto, $subject, $me
 	$mail->Port = 587;
 	$mail->SMTPAuth = true;
 	$mail->Username = "compreingressospedidos@hotmail.com";
-	$mail->Password = "743081clc2";
+	$mail->Password = "Clcruz121415";
 
 	$mail->SMTPSecure = "tls";
 	

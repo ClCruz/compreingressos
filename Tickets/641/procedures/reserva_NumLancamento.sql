bc483fb48e3cbccaf2c31fc364a71388e08ea19d
Gabriel Monteiro <gabriel.monteiro@intuiti.com.br>
2015-10-02 17:21:30 +0000

    #641
    git-svn-id: https://portal.cc.com.br:8084/svn/projetos@2726 236fd033-a1ae-4692-b96d-e3c3cfb81559

---
+++ Tickets/641/procedures/reserva_NumLancamento.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[reserva_NumLancamento](
+	[NumLancamento] [int] NOT NULL,
+ CONSTRAINT [PK_reserva_NumLancamento] PRIMARY KEY CLUSTERED 
+(
+	[NumLancamento] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]

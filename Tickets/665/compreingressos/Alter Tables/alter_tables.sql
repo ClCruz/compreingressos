e166654f597764866a6f9d8aeaa398f0bdb5043c
Edicarlos Barbosa <edicarlos.barbosa@intuiti.com.br>
2016-03-02 15:13:12 +0000

    Adicionado arquivos que estavam faltando.
    git-svn-id: https://portal.cc.com.br:8084/svn/projetos@2877 236fd033-a1ae-4692-b96d-e3c3cfb81559

---
+++ Tickets/665/compreingressos/Alter Tables/alter_tables.sql
@@ -0,0 +1,6 @@
+INSERT INTO TABCAIXA VALUES (250, 'C', 'A', 'POS', 6, 'POS');
+
+
+
+insert into tabCaixa (codcaixa, tipcaixa, StaCaixa, Maquina, id_canal_venda, DescrCaixa)
+values (249, 'C', 'A', 'VENDAS PDV-WEB', '4', 'VENDAS PDV-WEB');

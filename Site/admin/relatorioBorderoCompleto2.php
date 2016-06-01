98f1204cc046ad1d6dc269b4a50be9f201381df1
Felipe Barreiros <felipe.php@outlook.com>
2016-06-01 16:52:55 -0300

    #101 - Alterar queries. ao inves de pegar a primeira imagem do banco, pegar a imagem da primeira sala do Combo

--- Site/admin/relatorioBorderoCompleto2.php
+++ Site/admin/relatorioBorderoCompleto2.php
@@ -140,6 +140,7 @@ if (acessoPermitido($mainConnection, $_SESSION['admin'], 305, true)) {
 
             function validar()
             {
+                var primeiraSala = ''; //Primeiro option do combo após "SELECIONE" e "TODOS".
                 if(document.fPeca.cboPeca.value == "")
                 {
                     $.dialog({title: 'Alerta...',text: 'Selecione o evento'});
@@ -170,6 +171,10 @@ if (acessoPermitido($mainConnection, $_SESSION['admin'], 305, true)) {
                     document.fPeca.cboSala.focus();
                     return;
                 }
+                else
+                {
+                    primeiraSala = document.fPeca.cboSala.options[2].value;
+                }
 
                 if((document.fPeca.txtData1.value == ""
                     || document.fPeca.txtData2.value == "")
@@ -192,7 +197,7 @@ if (acessoPermitido($mainConnection, $_SESSION['admin'], 305, true)) {
                 var url = "relBorderoCompleto2.php";
                 url += "?CodPeca=" + document.fPeca.cboPeca.value;
                 url += "&logo=imagem";
-
+                url += "&fSala=" + primeiraSala;
                 url += "&Resumido=0";
                 url += "&Small=" + ((document.fPeca.chkSmall.checked) ? '1' : '0');
                 url += "&DataIni=" + data1;

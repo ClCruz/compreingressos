513ea95569a1a4d035ea427a93783d72cb5b1b94
Felipe Barreiros <felipe.php@outlook.com>
2016-06-02 14:35:24 -0300

    #105 - inserir alias na query

--- Site/settings/functions.php
+++ Site/settings/functions.php
@@ -2492,8 +2492,8 @@ function getSalaImg($codSala, $conn)
     
     $query = 'SELECT B.Imagem FROM tabLogoSala AS A
               INNER JOIN tabImagem AS B ON B.CodImagem = A.CodImagem 
-              WHERE CodSala = '.$codSala.' AND ExibirLogoBordero = 1';
-
+              WHERE A.CodSala = '.$codSala.' AND A.ExibirLogoBordero = 1';
+    
     $imagem = fetchAssoc( executeSQL($conn, $query) );
     $imagem = $imagem[0]['Imagem'];
 

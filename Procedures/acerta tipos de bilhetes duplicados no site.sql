be7fc3a04579963510bd4a8c2acc794875d5fef0
Jefferson Ferreira <jefferson.ferreira@intuiti.com.br>
2016-06-24 14:32:58 -0300

    script para retirar tipos de bilhetes duplicados do site

---
+++ Procedures/acerta tipos de bilhetes duplicados no site.sql
@@ -0,0 +1,55 @@
+Programador Ruby
+Intuiti Soluções em Tecnologia - São Paulo e Região, Brasil
+Descrição da vaga
+
+Programador com sólida experiêcia em Ruby, MySql e Linux;
+ 
+Contratação CLT + benefícios.
+ 
+Local de trabalho próximo à Praça da Republica, São Paulo/SP.
+ 
+Interessados enviar curriculum e pretensão salarial.
+Criar um cadastro básico de pessoa (nome, endereço, telefone, email) que seja desenvolvido com Ruby em Linux e enviar link para avaliação.
+ 
+ 
+ 
+Competências e experiência desejadas
+
+Linux
+Ruby
+MYSQL
+PHP
+Javascript
+HTML 
+CSS
+ 
+Descrição da empresa
+
+Empresa de tecnologia com foco em desenvolvimento de novas tecnologias com sólida base de clientes e em crescimento.
+Informações adicionais
+
+Tipo: Tempo integral Experiência: Pleno-sênior Funções: Tecnologia da informação Setores: Tecnologia da informação e serviços
+http://www.ondetrabalhar.com/vagas/5862
+
+quality suite alphiv
+
+imento
+adriana - AA005TO4LC
+kaiam - AA005TO6EW 
+http://www.usembassy.gov/
+http://portuguese.brazil.usembassy.gov/
+https://ais.usvisa-info.com/pt-br/niv -agendamento e tx visto
+
+TEMP. BUSINESS PLEASURE VISITOR (B) 
+ Specify: BUSINESS & TOURISM (TEMPORARY VISITOR) (B1/B2) 
+ 
+Intended Date of Arrival: 15 JULY 2016 
+Intended Length of Stay in U.S.: 11 DAY(S)  
+Address where you will stay in the U.S.: 5820 W IRLO BRONSON HIGHWAY, KISSIMMEE, 
+ ORLANDO, FLORIDA 34746 
+Person/Entity Paying for Your Trip: OTHER PERSON 
+Name of Person Paying for Your Trip: FERREIRA, JEFFERSON 
+Telephone Number: 551123350770 
+Email Address: fer.jeff@hotmail.com 
+Relationship to You: SPOUSE 
+Is the address of the party paying for your trip the same as your Home or Mailing Address? YES 

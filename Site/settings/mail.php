c4e46ddf6faf0192f1232a63487350fecdc7551a
Gabriel Fernandes Monteiro <gabriel.monteiro@intuiti.com.br>
2017-03-27 10:12:57 -0300

    nova configuração de contingência para envio de email

--- Site/settings/mail.php
+++ Site/settings/mail.php
@@ -109,7 +109,7 @@ function authSendEmail_alternativo($from, $namefrom, $to, $nameto, $subject, $me
 	$mail->Port = 587;
 	$mail->SMTPAuth = true;
 	$mail->Username = 'compreingressos@gmail.com';
-	$mail->Password = 'cruz79513579';
+	$mail->Password = 'Compr3Tub3';
 	
 	// somente gmail
 	$mail->From = 'compreingressos@gmail.com';
@@ -287,6 +287,92 @@ function authSendEmail_alternativo3($from, $namefrom, $to, $nameto, $subject, $m
 	
 	// somente gmail
 	$mail->From = ($from ? $from : 'compreingressos@siscompre.com');
+	$mail->FromName = $namefrom;
+	
+	$mail->AddAddress($to, $nameto);
+	//$mail->AddAddress('e-mail@destino2.com.br');
+	//$mail->AddCC('copia@dominio.com.br', 'Copia');
+	//$mail->AddBCC('CopiaOculta@dominio.com.br', 'Copia Oculta');
+	
+	if (!empty($copiesTo)) {
+		foreach($copiesTo as $address) {
+			$address = explode('=>', $address);
+			
+			$name = trim($address[0]);
+			$email = trim($address[1]);
+			
+			$mail->AddCC($email, $name);
+		}
+	}
+
+	if (!empty($hiddenCopiesTo)) {
+		foreach($hiddenCopiesTo as $address) {
+			$address = explode('=>', $address);
+			
+			$name = trim($address[0]);
+			$email = trim($address[1]);
+			
+			$mail->AddBCC($email, $name);
+		}
+	}
+
+	if (!empty($attachment)) {
+		foreach($attachment as $file) {
+			if ($file['cid']) {
+				$mail->AddEmbeddedImage($file['path'], $file['cid']);
+				//and on the <img> tag put src='cid:file_cid'
+			} else {
+				$mail->AddAttachment($file['path'], $file['new_name']);  
+			}
+		}
+	}
+	
+	$mail->IsHTML(true);
+	$mail->CharSet = $charset;
+	
+	$mail->Subject  = $subject;
+	$mail->Body = $message;
+	//$mail->AltBody = 'plain text';
+	
+	$enviado = $mail->Send();
+
+	log_email($mail->Subject, $mail->From, $to, $mail->Host, $enviado, $error);
+
+	if ($enviado) {
+		if (!empty($attachment)) {
+			foreach($attachment as $file) {
+				unlink($file['path']);
+			}
+			limparImagesTemp();
+		}
+
+		return true;
+	} else {
+		return authSendEmail_alternativo4($from, $namefrom, $to, $nameto, $subject, $message, $copiesTo, $hiddenCopiesTo, $charset, $attachment);
+	}
+}
+
+function authSendEmail_alternativo4($from, $namefrom, $to, $nameto, $subject, $message, $copiesTo = array(), $hiddenCopiesTo = array(), $charset = 'utf8', $attachment = array()) {
+	
+	$mail = new PHPMailer();
+	
+	$mail->SMTPDebug = 0;
+	
+	$mail->SetLanguage('br');
+	
+	$mail->IsSMTP();
+	$mail->Host = 'smtplw.com.br';
+
+	// somente gmail
+	// $mail->SMTPSecure = "tls";
+	
+	$mail->Port = 587;
+	$mail->SMTPAuth = true;
+	$mail->Username = 'cultingressos1';
+	$mail->Password = 'xhRSWnoW1042';
+	
+	// somente gmail
+	$mail->From = 'compreingressos@compreingressos.com';
 	$mail->FromName = $namefrom;
 	
 	$mail->AddAddress($to, $nameto);

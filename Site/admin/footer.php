87a01bfe9844238c8c31313407999de64112a973
Ítalo Patrício <italopithu@hotmail.com>
2017-06-02 16:26:30 -0300

    Adição de módulo Gerencia de Cinema

--- Site/admin/footer.php
+++ Site/admin/footer.php
@@ -1,5 +1,5 @@
     <div id='footer' class='ui-corner-bottom'>
-        <p style='font-weight:bold; float:right; clear:right; margin-right:15px;'><a href='http://www.cc.com.br'>by C&amp;C</a></p>
+        <p style='font-weight:bold; float:right; clear:right; margin-right:15px;'><a href='http://www.intuiti.com.br'>by Intuiti</a></p>
     	<p style='font-style:italic; margin-left:15px'>2010 &copy; - Todos os direitos reservados</p>
     </div>
 </div>

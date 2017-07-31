9e8ae18319fc56e51b225bc9ea25740b5ec47c53
Gabriel Fernandes Monteiro <gabriel.monteiro@intuiti.com.br>
2017-07-31 16:49:44 -0300

    #404 notificação por email no caso de venda com sucesso e ingressos com código de venda XXX

--- Site/comprar/concretizarCompra.php
+++ Site/comprar/concretizarCompra.php
@@ -69,6 +69,28 @@ if ($noErrors and empty($sqlErrors)) {
 	include('logiPagareChamada.php');
 
 	sendSuccessMail($pedido_id);
-} else {
-	include('errorMail.php');
+}
+
+
+// checar se tem XXX na item - erro que nao conseguimos identificar como ocorre (talvez alguma falha com id session no banco)
+$rs = executeSQL($mainConnection, "SELECT COUNT(1) FROM MW_ITEM_PEDIDO_VENDA WHERE ID_PEDIDO_VENDA = ? AND CODVENDA = 'XXXXXXXXXX'", array($pedido_id), true);
+$enviar_email_erro_XXX = $rs[0] > 0;
+
+if ( $enviar_email_erro_XXX OR !($noErrors and empty($sqlErrors)) ) {
+
+	//define the body of the message.
+	ob_start(); //Turn on output buffering
+	?>
+	<p>&nbsp;</p>
+	<p>XXXXXXXXXX</p>
+	<p>&nbsp;</p>
+	<p>Dump de variaveis:</p>
+	<p><pre><?php print_r(get_defined_vars()); ?></pre></p>
+	<p>&nbsp;</p>
+	<?php
+	//copy current buffer contents into $message variable and delete current output buffer
+	$message = ob_get_clean();
+
+	sendErrorMail('Atenção: Possibilidade de venda em duplicidade - COMPREINGRESSOS.COM', $message);
+
 }

68c0cee071741c8f4b32496b5b142ca9cad963ee
Everton Silva <thonns@gmail.com>
2017-10-04 16:03:22 -0300

    1604 - mudar 'peso' da font Source Sans Pro de 900 para 700

--- Site/comprar/confirmacaoEmail.php
+++ Site/comprar/confirmacaoEmail.php
@@ -44,7 +44,7 @@
 	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 	<meta name="robots" content="noindex,nofollow">
 	<link href="../images/favicon.ico" rel="shortcut icon"/>
-	<link href='https://fonts.googleapis.com/css?family=Paprika|Source+Sans+Pro:200,400,400italic,200italic,300,900' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Paprika|Source+Sans+Pro:200,400,400italic,200italic,300,700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="../stylesheets/cicompra.css"/>
 	<link rel="stylesheet" href="../stylesheets/ajustes2.css"/>
 	<link rel="stylesheet" type="text/css" href="../stylesheets/nova_home.css">
--- Site/comprar/minha_conta.php
+++ Site/comprar/minha_conta.php
@@ -158,7 +158,7 @@ if (isset($_SESSION['user']) and is_numeric($_SESSION['user'])) {
         <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
         <meta name="robots" content="noindex,nofollow"/>
         <link href="../images/favicon.ico" rel="shortcut icon"/>
-        <link href='https://fonts.googleapis.com/css?family=Paprika|Source+Sans+Pro:200,400,400italic,200italic,300,900' rel='stylesheet' type='text/css'/>
+        <link href='https://fonts.googleapis.com/css?family=Paprika|Source+Sans+Pro:200,400,400italic,200italic,300,700' rel='stylesheet' type='text/css'/>
         <link rel="stylesheet" href="../stylesheets/cicompra.css"/>
         
         <link rel="stylesheet" href="../stylesheets/ajustes2.css"/>

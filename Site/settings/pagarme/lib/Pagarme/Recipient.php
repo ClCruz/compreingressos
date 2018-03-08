ae68fa29ad3f90b0a565b26e54e8d3009ee907a9
Edicarlos Barbosa <edicarlos.barbosa@intuiti.com.br>
2018-03-08 14:24:59 -0300

    #2525 - Inclui funcionalidade de Extrato do Recebedor

--- Site/settings/pagarme/lib/Pagarme/Recipient.php
+++ Site/settings/pagarme/lib/Pagarme/Recipient.php
@@ -1,4 +1,29 @@
 <?php
 
 class PagarMe_Recipient extends PagarMe_Model {
+
+	const ENDPOINT_RECIPIENTS = '/recipients';
+
+	public static function findAllByRecipientId($recipientId)
+	{
+		$request = new PagarMe_Request(
+            self::ENDPOINT_RECIPIENTS . '/' . $recipientId . '/balance/operations', 'GET'
+        );
+
+        $response = $request->run();
+        $class = get_called_class();
+        return new $class($response);
+	}
+
+	public static function findSaldoByRecipientId($recipientId)
+	{
+		$request = new PagarMe_Request(
+            self::ENDPOINT_RECIPIENTS . '/' . $recipientId . '/balance', 'GET'
+        );
+
+        $response = $request->run();
+        $class = get_called_class();
+        return new $class($response);
+	}
+
 }

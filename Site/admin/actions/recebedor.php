6452a3ad6e995b62708b56c98cafb05359df24bc
Edicarlos Barbosa <edicarlos.barbosa@intuiti.com.br>
2018-03-08 14:13:05 -0300

    #2584 - Inclui funcionalidade de cadastro de recebedor

---
+++ Site/admin/actions/recebedor.php
@@ -0,0 +1,163 @@
+<?php
+
+require('../settings/pagarme_functions.php');
+
+if (acessoPermitido($mainConnection, $_SESSION['admin'], 660, true)) {
+
+	if ($_GET['action'] != 'delete') {
+		$ddd_celular = trim(substr($_POST['celular'], 0, 3));
+        $celular = str_replace("-","",substr($_POST['celular'], 3, 10));
+        $ddd_telefone = (trim($_POST['telefone']) != "") ? substr(str_replace("-","", trim($_POST['telefone'])), 0, 2) : "";
+        $telefone = (trim($_POST['telefone']) != "") ? substr(str_replace("-","", trim($_POST['telefone'])), 2, 9) : "";
+	}
+
+	if ($_GET['action'] == 'add') {
+
+		$query = "EXEC SP_REC_INS001 ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?";
+
+		$params = array(strtoupper(utf8_decode(trim($_POST["razao_social"]))), 
+						$_POST["cpf_cnpj"],
+						ucwords(utf8_decode(trim($_POST["nome"]))), 
+						trim(strtolower($_POST["email"])), 
+						trim($ddd_telefone),
+						trim($telefone),
+						trim($ddd_celular),
+						trim($celular),
+						$_POST["banco"], 
+						trim($_POST["agencia"]),
+						trim($_POST["dv_agencia"]),
+						trim($_POST["conta_bancaria"]), 
+						trim($_POST["dv_conta_bancaria"]), 
+						$_POST["tipo"],
+						$_GET["produtor"],
+						$_POST["status"]);
+
+		$rs = executeSQL($mainConnection, $query, $params, true);
+		
+		$recipient = salvarContaBancariaPagarme($_POST, $_GET["produtor"]);
+
+		$query = "UPDATE mw_recebedor SET recipient_id = ? WHERE id_recebedor = ?";
+		$param = array($recipient["id"], $rs["id"]);
+		executeSQL($mainConnection, $query, $param);
+
+		$retorno = 'true?id=' . $rs["id"];
+
+		if(sqlErrors()) {
+			$retorno = sqlErrors();
+		}
+	} else if ($_GET['action'] == 'update' and isset($_GET['id'])) {
+
+		$query = "UPDATE mw_recebedor
+				  SET ds_razao_social = ?,
+				  	  cd_cpf_cnpj = ?,
+				  	  ds_nome = ?,
+				  	  cd_email = ?,
+				  	  ds_ddd_telefone = ?,
+				  	  ds_telefone = ?,
+				  	  ds_ddd_celular = ?,
+				  	  ds_celular = ?,
+				  	  cd_banco = ?, 
+					  cd_agencia = ?,
+					  dv_agencia = ?,
+					  cd_conta_bancaria = ?,
+					  dv_conta_bancaria = ?,
+					  cd_tipo_conta = ?,
+					  id_produtor = ?,
+					  in_ativo = ?
+				  WHERE id_recebedor = ?";
+
+		$params = array(strtoupper(utf8_decode(trim($_POST["razao_social"]))), 
+						$_POST["cpf_cnpj"],
+						ucwords(utf8_decode(trim($_POST["nome"]))), 
+						trim(strtolower($_POST["email"])), 
+						trim($ddd_telefone),
+						trim($telefone),
+						trim($ddd_celular),
+						trim($celular),
+						$_POST["banco"], 
+						trim($_POST["agencia"]),
+						trim($_POST["dv_agencia"]),
+						trim($_POST["conta_bancaria"]), 
+						trim($_POST["dv_conta_bancaria"]), 
+						$_POST["tipo"],
+						$_GET["produtor"],
+						$_POST["status"],
+						$_GET['id']);
+
+		if (executeSQL($mainConnection, $query, $params)) {
+            $retorno = 'true?id=' . $_GET['id'];
+        } else {
+            $retorno = sqlErrors();
+        }
+
+	} else if ($_GET['action'] == 'delete' and isset($_GET['id'])) { /* ------------ DELETE ------------ */
+
+		$query = 'UPDATE mw_recebedor SET in_ativo = 0 WHERE id_recebedor = ?';
+        $params = array($_GET['id']);
+        
+        if (executeSQL($mainConnection, $query, $params)) {
+            $retorno = 'true';
+        } else {
+            $retorno = sqlErrors();
+        }
+
+	 } else if ($_GET['action'] == 'load' and isset($_GET['id'])){
+		$query = 'SELECT
+                   id_recebedor,
+                   ds_razao_social,
+                   cd_cpf_cnpj,
+                   ds_nome,
+                   cd_email,
+                   ds_ddd_telefone,
+                   ds_telefone,
+                   ds_ddd_celular,
+                   ds_celular,
+                   cd_banco,
+                   cd_agencia,
+                   dv_agencia,
+                   cd_conta_bancaria,
+                   dv_conta_bancaria,
+                   cd_tipo_conta,
+                   id_produtor,
+                   in_ativo
+                  FROM mw_recebedor WHERE id_recebedor = ?';
+        $params = array($_GET['id']);
+        $result = executeSQL($mainConnection, $query, $params);
+
+        while ($rs = fetchResult($result)) {            
+            $ret = array(
+            	"id" => $rs["id_recebedor"],
+            	"razao_social" => utf8_encode($rs["ds_razao_social"]),
+            	"cpf_cnpj" => $rs["cd_cpf_cnpj"],
+            	"nome" => utf8_encode($rs["ds_nome"]),
+            	"email" => $rs["cd_email"],
+            	"telefone" => $rs["ds_ddd_telefone"] ." ". substr($rs["ds_telefone"], 0, 4) ."-". substr($rs["ds_telefone"], 4, 5),
+            	"celular" => $rs["ds_ddd_celular"] ." ". substr($rs["ds_celular"], 0, 4) ."-". substr($rs["ds_celular"], 4, 5),
+            	"banco" => $rs["cd_banco"],         
+            	"agencia" => $rs["cd_agencia"],
+            	"dv_agencia" => $rs["dv_agencia"],
+            	"conta_bancaria" => $rs["cd_conta_bancaria"],
+            	"dv_conta_bancaria" => $rs["dv_conta_bancaria"],
+            	"tipo" => $rs["cd_tipo_conta"],
+            	"status" => $rs["in_ativo"]
+            );
+        }
+        $retorno = json_encode($ret);
+
+    } else if ($_GET['action'] == 'check' and isset($_GET['produtor'])){
+    	$query = "SELECT SUM(nr_percentual_split) AS split FROM mw_conta_bancaria WHERE id_produtor = ? AND in_ativo = 1 AND (id_conta_bancaria != ? OR ? = -1)";
+    	$param = array($_GET["produtor"], $_GET["conta"], $_GET["conta"]);
+    	$stmt  = executeSQL($mainConnection, $query, $param, true);
+    	$retorno = (!isset($stmt["split"]) || $stmt["split"] == null) ? 0 : $stmt["split"];
+	} else {
+		$retorno = "Nenhuma ação executada.";
+	}
+
+	if (is_array($retorno)) {
+        echo $retorno[0]['message'];
+    } else {
+        echo $retorno;
+    }
+}
+
+?>
---
+++ Site/admin/recebedor.php
@@ -0,0 +1,395 @@
+<?php
+require_once('acessoLogadoDie.php');
+require_once('../settings/functions.php');
+
+$mainConnection = mainConnection();
+session_start();
+
+if (acessoPermitido($mainConnection, $_SESSION['admin'], 660, true)) {
+
+    $pagina = basename(__FILE__);
+
+    if (isset($_GET['action']) or isset($_POST['codigo'])) {
+        
+        require('actions/'.$pagina);
+        
+    } else {
+
+        $query = "SELECT cd_banco, ds_banco FROM mw_banco ORDER BY ds_banco";
+        $stmtBanco = executeSQL($mainConnection, $query);
+
+    	$query = "SELECT * FROM mw_recebedor cb 
+                  INNER JOIN mw_banco b ON b.cd_banco = cb.cd_banco 
+                  WHERE id_produtor = ? AND in_ativo = 1 
+                  ORDER BY ds_razao_social";
+    	$stmt = executeSQL($mainConnection, $query, array($_GET["produtor"]));
+?>
+<style type="text/css">
+	label, input { display:block; }
+    #conta input.text, #conta select { margin-bottom:12px; width:95%; padding: .4em; }
+    fieldset { padding:10px; margin-top:10px; }
+    .td-action {text-align: center; width: 50px;}
+    .th-action {text-align: center; width: 100px;}
+    .add-conta {margin-bottom: 20px; text-align: right;}
+    .add-conta label {display: inline; float: left; margin-top: 7px;}
+    .add-conta select {margin-top: 5px; margin-bottom: 0px;}
+    .conta {width: 200px !important; display: inline;}
+    .conta-dv {width: 50px !important; display: inline;}
+    .agencia {width: 200px !important; display: inline;}
+    .agencia-dv {width: 50px !important; display: inline;}
+    #produtor {float: left; width: unset !important; padding: unset;}
+    #app h2, .appExtension h2 {margin: 15px 0px 15px 0px;}
+    .ui-dialog{ padding: .3em; }
+    .validateTips { border: 1px solid transparent; padding: 0.3em; }
+    #app #new {margin: 0px;}
+    .text-left{text-align: left;}
+    .text-right{text-align: right;}
+</style>
+<script type="text/javascript" src="../javascripts/simpleFunctions.js"></script>
+<script src="../javascripts/jquery.maskedinput.min.js" type="text/javascript"></script>
+<script>
+$(function() {
+	var pagina = '<?php echo $pagina; ?>';
+	var dialog, 
+		form,
+		emailRegex  = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+        id          = $("#id"),
+        produtor    = $("#produtor"),
+        id_produtor = $("#id_produtor"),
+        razao_social= $("#razao_social"),
+        cpf_cnpj    = $("#cpf_cnpj"),
+        nome        = $("#nome"),
+        email       = $("#email"),
+        telefone    = $("#telefone"),
+        celular     = $("#celular"),
+        banco       = $("#banco"),
+        conta       = $("#conta_bancaria"),
+        dv_conta_bancaria = $("#dv_conta_bancaria"),
+        agencia     = $("#agencia"),
+        dv_agencia  = $("#dv_agencia"),
+        tipo        = $("#tipo"),
+        split       = $("#split"),
+        status      = $("#status"),
+        allFields   = $([]).add(produtor).add(razao_social).add(cpf_cnpj).add(nome).add(email).add(celular).add(banco).add(conta).add(dv_conta_bancaria).add(dv_agencia).add(agencia).add(tipo).add(split),
+        tips        = $(".validateTips");
+
+	$('.button').button();
+
+    $("#telefone").mask("99 9999-9999");
+    $('#celular').mask("99 9999-9999?9");
+
+    $("#agencia").keypress(verificaNumero);
+    $("#dv_agencia").keypress(verificaNumero);
+    $("#conta_bancaria").keypress(verificaNumero);
+    $("#dv_conta_bancaria").keypress(verificaNumero);
+    $("#split").keypress(verificaNumero);
+    $("#cpf_cnpj").keypress(verificaNumero);
+
+    $('#app table').delegate('a', 'click', function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+        href = $this.attr('href'),
+        id = 'id=' + $.getUrlVar('id', href),
+        tr = $this.closest('tr');
+
+        if (href.indexOf('?action=edit') != -1) {
+        	$.get('recebedor.php?action=load&' + id, function(data) {
+            	data = $.parseJSON(data);
+
+            	$("#id").val(data.id);
+            	$("#razao_social").val(data.razao_social);
+                $("#cpf_cnpj").val(data.cpf_cnpj);
+                $("#nome").val(data.nome);
+                $("#email").val(data.email);
+                $("#telefone").val(data.telefone);
+                $("#celular").val(data.celular);
+                $("#banco").val(data.banco);
+            	$("#conta_bancaria").val(data.conta_bancaria);
+                $("#dv_conta_bancaria").val(data.dv_conta_bancaria);
+            	$("#agencia").val(data.agencia);
+                $("#dv_agencia").val(data.dv_agencia);
+            	$("#tipo").val(data.tipo);
+            	$("#split").val(data.split);
+            	$("#status").val(data.status);
+
+            	dialog.dialog( "open" );
+            });
+        }  else if (href.indexOf('?action=delete') != -1) {
+        	$.confirmDialog({
+                text: 'Tem certeza que deseja apagar este registro?',
+                uiOptions: {
+                    buttons: {
+                        'Sim': function() {
+                            $(this).dialog('close');
+                            $.get(href, function(data) {
+                                if (data.replace(/^\s*/, "").replace(/\s*$/, "") == 'true') {
+                                    tr.remove();
+                                } else {
+                                    $.dialog({text: data});
+                                }
+                            });
+                        }
+                    }
+                }
+            });
+        }
+    });
+
+	function updateTips( t ) {
+	    tips
+	    .text( t )
+	    .addClass( "ui-state-highlight" );
+	    setTimeout(function() {
+	        tips.removeClass( "ui-state-highlight", 1500 );
+	    }, 500 );
+	}	
+	
+	function checkRegexp( o, regexp, n ) {
+	    if ( !( regexp.test( o.val() ) ) ) {
+	        o.addClass( "ui-state-error" );
+	        updateTips( n );
+	        return false;
+	    } else {
+	        return true;
+	    }
+	}
+
+	dialog = $( "#dialog-form" ).dialog({
+        autoOpen: false,
+        height: 600,
+        width: 600,
+        modal: true,
+        buttons: {
+            "Salvar": add,
+            Cancelar: function() {
+                dialog.dialog( "close" );
+            }
+        },
+        close: function() {
+            document.forms[1].reset();
+            id.val("");
+            tips.text("");
+            tips.removeClass( "ui-state-highlight");
+            allFields.removeClass( "ui-state-error" );
+        }
+    });
+
+    function check(split, conta) {
+        valid = true;
+
+        conta = (conta == null || conta == "") ? -1 : conta;
+
+        $.ajax({
+            url: 'recebedor.php',
+            async: false,
+            type: 'get',
+            data: 'action=check&produtor='+ $('#produtor').val() + '&conta='+ conta,
+            success: function(data) {
+                soma = parseInt(data) + parseInt(split);
+                if(soma > 100) {                
+                    valid = false;
+                }
+            }
+        });
+
+        return valid;
+    }
+
+    function add() {
+    	var valid = true;
+        allFields.removeClass( 'ui-state-error' );
+        $.each(allFields, function() {
+            var $this = $(this);
+            if ($this.val() == '') {
+                $this.addClass('ui-state-error');
+                valid = false;
+            } else {
+                $this.removeClass('ui-state-error');
+            }
+        });        
+
+        if(split.val() > 100) {
+            tips.text("O valor do Split não pode ser maior do que 100.").addClass( "ui-state-highlight" );
+            split.addClass("ui-state-error");
+            valid = false;
+        }
+
+        if(!check(split.val(), id.val())) {
+            tips.text("O valor do Split na soma das contas não pode ser maior do que 100.").addClass( "ui-state-highlight" );
+            split.addClass("ui-state-error");
+            valid = false;
+        }
+
+        if( !verificaCPF( cpf_cnpj.val() ) && !verificaCNPJ( cpf_cnpj.val() ) ){
+            valid = false;
+            updateTips ("CPF / CNPJ inválido!");
+            cpf_cnpj.addClass('ui-state-error');
+        }
+
+        valid = valid && checkRegexp( email, emailRegex, "E-mail inválido!" );        
+
+        if ( valid ) {
+        	if ( id.val() == "" ){
+                var p = 'recebedor.php?action=add&produtor='+ produtor.val();
+            }else{
+                var p = 'recebedor.php?action=update&id='+ id.val() +'&produtor='+ produtor.val();
+            }
+
+            $.ajax({
+				url: p,
+				type: 'post',
+				data: $('#conta').serialize(),
+				success: function(data) {
+					if (trim(data).substr(0, 4) == 'true') {
+                        location.reload();
+                    } else {
+                        $.dialog({text: data});
+                    }
+				},
+				error: function(){
+                    $.dialog({
+                        title: 'Erro...',
+                        text: 'Erro na chamada dos dados !!!'
+                    });
+                    return false;
+                }
+			});
+			dialog.dialog( "close" );
+        }
+        return valid;
+    }
+
+    $('#new').button().click(function(event) {
+    	event.preventDefault();
+        if($("#produtor").val() == -1) {
+            $.dialog({
+                title: 'Alerta...',
+                text: 'Selecione o Produtor!'
+            });
+        } else {
+            dialog.dialog( "open" );    
+        }        
+    });
+
+    form = dialog.find( "form" ).on( "submit", function( event ) {
+        event.preventDefault();
+        add();
+    });
+
+    $("#produtor").change(function() {
+        location.href = "?p=recebedor&produtor=" + $(this).val();
+    })
+
+});
+</script>
+<h2>Recebedores</h2>
+
+<div id="dialog-form" title="Informações do Recebedor">
+	<p class="validateTips"></p>
+	<form id="conta" name="conta" action="?p=recebedor" method="POST">
+		<input type="hidden" name="id" id="id" value="" />
+        <input type="hidden" name="id_produtor" id="id_produtor" value="" />
+        
+        <fieldset>
+            <legend>Dados do Recebedor</legend>
+            <label for="razao_social">Razão Social:</label>
+            <input type="text" id="razao_social" name="razao_social" maxlength="250" class="text ui-widget-content ui-corner-all" />
+            <label for="cpf_cnpj">CPF / CNPJ:</label>
+            <input type="text" id="cpf_cnpj" name="cpf_cnpj" maxlength="14" class="text ui-widget-content ui-corner-all" />
+            <label for="nome">Nome:</label>
+            <input type="text" id="nome" name="nome" maxlength="100" class="text ui-widget-content ui-corner-all" />
+            <label for="email">E-mail:</label>
+            <input type="text" id="email" name="email" maxlength="100" class="text ui-widget-content ui-corner-all"/>
+            <label for="telefone">Telefone:</label>
+            <input type="text" id="telefone" name="telefone" maxlength="10" class="text ui-widget-content ui-corner-all" />
+            <label for="celular">Celular:</label>
+            <input type="text" id="celular" name="celular" maxlength="10" class="text ui-widget-content ui-corner-all" />
+        </fieldset>
+
+        <fieldset>
+            <legend>Informacoes da Conta</legend>
+		    <label for="banco">Banco:</label>		    
+            <select id="banco" name="banco" class="ui-widget-content ui-corner-all" />
+		    <?php while($rs = fetchResult($stmtBanco)) { ?>
+            <option value="<?php echo $rs['cd_banco']; ?>"><?php echo utf8_encode($rs['ds_banco']); ?></option>
+            <?php } ?>
+            </select>            
+            <label for="agencia">Agência:</label>
+		    <input type="text" id="agencia" name="agencia" maxlength="5" class="text ui-widget-content ui-corner-all agencia" /> - 
+            <input type="text" id="dv_agencia" name="dv_agencia" maxlength="1" class="text ui-widget-content ui-corner-all agencia-dv" />
+		    <label for="conta_bancaria">Conta:</label>
+		    <input type="text" id="conta_bancaria" name="conta_bancaria" maxlength="13" class="text ui-widget-content ui-corner-all conta" /> - 
+            <input type="text" id="dv_conta_bancaria" name="dv_conta_bancaria" maxlength="1" class="text ui-widget-content ui-corner-all conta-dv" />
+		    <label for="tipo">Tipo da Conta:</label>
+			<select id="tipo" name="tipo" class="ui-widget-content ui-corner-all">
+                <option value="CC">Conta Corrente</option>
+                <option value="CP">Conta Poupança</option>
+            </select>
+		    <label for="status">Status:</label>
+            <select id="status" name="status" class="ui-widget-content ui-corner-all">
+                <option value="1">Ativo</option>
+                <option value="0">Inativo</option>
+            </select>
+		</fieldset>
+	</form>
+</div>
+
+<form id="dados" name="dados" method="post">	
+	<div class="add-conta">
+        <label>Produtor: </label>
+        <select id="produtor" name="produtor">
+            <option value="-1">Selecione</option>
+            <?php
+                $query = "SELECT id_produtor, ds_razao_social FROM mw_produtor WHERE in_ativo = 1 ORDER BY ds_razao_social";
+                $stmtProdutor = executeSQL($mainConnection, $query);
+                while($rs = fetchResult($stmtProdutor)) {
+                    $selected = $rs["id_produtor"] == $_GET["produtor"] ? "selected" : "";
+            ?>
+            <option <?php echo $selected; ?> value="<?php echo $rs['id_produtor']; ?>"><?php echo utf8_encode($rs['ds_razao_social']); ?></option>
+            <?php
+                }
+            ?>
+        </select>
+		<a id="new" href="#new">Novo</a>	
+	</div>
+</form>
+
+<table class="ui-widget ui-widget-content">
+	<thead>
+		<tr class="ui-widget-header">
+			<th class="text-left">Nome</th>
+			<th class="text-left">Banco</th>
+			<th class="text-right">Agência</th>
+			<th class="text-right">Conta</th>
+			<th class="text-left">Tipo da Conta</th>
+            <th class="text-left">Recebedor (Pagar.me)</th>
+            <th class="text-left">Status</th>                
+			<th colspan="2" class="th-action">Ações</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php while($rs = fetchResult($stmt)) { 
+			$id = $rs["id_recebedor"];
+            $conta = ($rs["dv_conta_bancaria"] != "") ? $rs["cd_conta_bancaria"] ."-". $rs["dv_conta_bancaria"] : $rs["cd_conta_bancaria"];
+            $agencia = ($rs["dv_agencia"] != "") ? $rs["cd_agencia"] ."-". $rs["dv_agencia"] : $rs["cd_agencia"];
+		?>
+		<tr>
+			<td class="text-left"><?php echo utf8_encode($rs["ds_nome"]); ?></td>
+			<td class="text-left"><?php echo utf8_encode($rs["ds_banco"]); ?></td>
+			<td class="text-right"><?php echo $agencia; ?></td>
+			<td class="text-right"><?php echo $conta; ?></td>
+			<td class="text-left"><?php echo $rs["cd_tipo_conta"] == "CC" ? "Conta Corrente" : "Conta Poupança"; ?></td>
+            <td class="text-left"><?php echo $rs["recipient_id"]; ?></td>
+            <td class="text-left"><?php echo $rs["in_ativo"] ? "Ativo" : "Inativo"; ?></td>
+			<td class="td-action"><a href="<?php echo $pagina; ?>?action=edit&id=<?php echo $id; ?>" class="button">Editar</a></td>
+            <td class="td-action"><a href="<?php echo $pagina; ?>?action=delete&id=<?php echo $id; ?>" class="button">Apagar</a></td>
+		</tr>
+		<?php } ?>
+	</tbody>
+</table>
+<br/>
+<?php
+	}
+}
+?>

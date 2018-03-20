58aa500526ae1cf341e23be168acbe24480de009
Bruno Luigi Coccaro <blcoccaro@gmail.com>
2018-03-20 12:26:16 -0300

    Alterações referente a tela de login

---
+++ Site/admin/footer_new.php
@@ -0,0 +1,14 @@
+
+<!--Footer-->
+<footer class="page-footer font-small pt-4 mt-4">
+
+    <!--Copyright-->
+    <div class="footer-copyright py-3 text-center">
+        © 2018 Copyright:
+        <a href="http://www.intuiti.com.br"> Intuiti.com.br </a>
+    </div>
+    <!--/.Copyright-->
+
+</footer>
+<!--/.Footer-->
+                      
